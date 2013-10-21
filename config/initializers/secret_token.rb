5e541d2d08d4656928e17db8615243a17425abec
Despo Pentara <despo@extractmethod.com>
2013-10-21 21:37:17 +0100

    secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,5 @@
-# Be sure to restart your server when you modify this file.
+key = Rails.env.eql?("production") ? ENV['PLANNER_SECRET'] : "sample-key"
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+raise "No secret!" if key.nil?
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Planner::Application.config.secret_key_base = '9e72768eed725572fdf57f67fa273b68291f746f68f3411f9ea84ef3b0258c58c6a2acd5d2677b90546c75a4e115b71a4ea456d5081e5c278688909ad6cca76f'
+Planner::Application.config.secret_key_base = key
