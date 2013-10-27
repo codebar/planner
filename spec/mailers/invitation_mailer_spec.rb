8384e8f5a80d867c3c68f8ad4eb88f429af76a99
Despo Pentara <despo@extractmethod.com>
2013-10-27 17:09:20 +0000

    Update email template for this week's invitations

--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -9,7 +9,7 @@ describe InvitationMailer do
     sessions = Fabricate(:sessions)
     invitation_token = "token"
 
-    email_subject = "Post Rails Workshop sessions / HTML by Codebar - Wednesday Oct 23rd, 18:30"
+    email_subject = "HTML by Codebar - Wednesday Oct 30th, 18:30"
     InvitationMailer.invite(sessions, member, invitation_token).deliver
 
     expect(email.subject).to eq(email_subject)
