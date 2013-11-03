c6e86dd15175a2f6637988ca71440495337ada93
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2013-11-03 18:47:40 +0000

    Removes cyclic dependence between Address and Sponsor

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,3 @@
 class Address < ActiveRecord::Base
   belongs_to :sponsor
-
-  validates :sponsor_id, presence: true
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -7,10 +7,4 @@ describe Address do
   it { should respond_to(:flat) }
   it { should respond_to(:street) }
   it { should respond_to(:postal_code) }
-
-  context 'sponsor not present' do
-    before { address.sponsor = nil }
-
-    it{ should_not be_valid }
-  end
 end
