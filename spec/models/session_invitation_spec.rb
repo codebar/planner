fed1ab0ed840ce0a24109f2e5cced48d7471f333
Despo Pentara <despo@extractmethod.com>
2013-11-04 04:07:15 +0000

    improve tests

--- spec/models/session_invitation_spec.rb
+++ spec/models/session_invitation_spec.rb
@@ -6,4 +6,13 @@ describe SessionInvitation do
 
   it_behaves_like InvitationConcerns
 
+  it "#send_reminder" do
+    invitation.update_attribute(:attending, true)
+    mailer = mock(SessionInvitationMailer, deliver: nil)
+
+    SessionInvitationMailer.should_receive(:remind_student).
+      with(invitation.sessions, invitation.member, invitation).and_return(mailer)
+
+    invitation.send_reminder
+  end
 end
