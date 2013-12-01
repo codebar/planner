c9719f644966bc1b4af633a31829d23bdd74cd09
LukasMac <lukas.maciulis@gmail.com>
2013-12-01 10:21:49 +0000

    Add tutorial and feedback seeds

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,4 @@
 class Feedback < ActiveRecord::Base
   belongs_to :tutorial
-  belongs_to :coach
+  belongs_to :coach, class_name: "Member"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,13 @@ if Rails.env.development?
 
   sessions = 10.times.map { |n| Fabricate(:sessions, date_and_time: DateTime.now+14.days-n.weeks) }
 
-  courses = 5.times { |n| Fabricate(:course, date_and_time: DateTime.now+14.days-n.weeks) }
+  courses = 5.times.map { |n| Fabricate(:course, date_and_time: DateTime.now+14.days-n.weeks) }
 
   coaches = 20.times.map { |n| Fabricate(:coach) }
+
+  tutorials = 10.times.map { |n| Fabricate(:tutorial, course: courses.sample) }
+
+  feedbacks = 5.times { Fabricate(:feedback, tutorial: tutorials.sample, coach: coaches.sample) }
 
   40.times do |n|
     coach = coaches.sample
