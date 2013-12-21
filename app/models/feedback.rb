50bfe598caeef4f4cf03a9aad25251f7a5735cd5
LukasMac <lukas.maciulis@gmail.com>
2013-12-21 12:41:13 +0000

    Add feedback model and specs

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,16 @@
 class Feedback < ActiveRecord::Base
   belongs_to :tutorial
-  belongs_to :coach, class_name: "Member"
+  belongs_to :coach, class_name: "Member" 
+
+  validates :coach, presence: true
+  validate :coach_field_has_a_coach_role?
+
+  def coach_field_has_a_coach_role?
+  	return false unless coach_id
+
+  	coach_roles = Member.find(coach_id).roles.select { |role| role.name == 'Coach' }
+  	if coach_roles.empty?
+  		errors.add(:coach, "Coach member doesn't have 'coach' role.")
+  	end
+  end
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -7,4 +7,27 @@ describe Feedback do
   it { should respond_to(:suggestions) }
   it { should respond_to(:coach) }
   it { should respond_to(:tutorial)}
+
+  context "validations" do
+    it "#coach accepts memeber with 'coach' role" do
+      feedback = Fabricate.build(:feedback, coach: Fabricate(:coach))
+
+      feedback.should be_valid
+    end
+
+    it "#coach is invalid with memeber with 'student' role" do
+      feedback = Fabricate.build(:feedback, coach: Fabricate(:student))
+
+      feedback.should_not be_valid
+      feedback.should have(1).error_on(:coach)
+    end
+
+    it "#coach should display correct error message when member with 'student' role given" do
+      feedback = Fabricate.build(:feedback, coach: Fabricate(:student))
+
+      feedback.valid?
+
+      feedback.errors.messages[:coach].should include "Coach member doesn't have 'coach' role."
+    end
+  end
 end
