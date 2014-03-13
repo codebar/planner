78627865e623dfe33cf34677ba5627a345b5b9cc
Despo Pentara <despo@extractmethod.com>
2014-03-13 23:25:52 +0000

    set_token before validation takes place and make sure each request is only send once!

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,6 @@
 class Feedback < ActiveRecord::Base
   belongs_to :tutorial
-  belongs_to :coach, class_name: "Member" 
+  belongs_to :coach, class_name: "Member"
 
   validates :rating, inclusion: { in: 1..5, message: "can't be blank" }
   validates :coach, presence: true
@@ -8,12 +8,12 @@ class Feedback < ActiveRecord::Base
   validate :coach_field_has_a_coach_role?
 
   def coach_field_has_a_coach_role?
-  	return false unless coach_id
+    return false unless coach_id
 
-  	coach_roles = Member.find(coach_id).roles.select { |role| role.name == 'Coach' }
-  	if coach_roles.empty?
-  		errors.add(:coach, "Coach member doesn't have 'coach' role.")
-  	end
+    coach_roles = Member.find(coach_id).roles.select { |role| role.name == 'Coach' }
+    if coach_roles.empty?
+      errors.add(:coach, "Coach member doesn't have 'coach' role.")
+    end
   end
 
   def self.submit_feedback params, token
