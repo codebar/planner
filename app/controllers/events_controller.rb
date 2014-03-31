3270b8bdcca04d42fc16eb3d5cfa9d9c33c4f07b
Despo Pentara <despo@extractmethod.com>
2014-03-31 23:42:28 +0100

    it's 2014!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,12 +4,12 @@ class EventsController < ApplicationController
     @past_events = Sessions.past
     @past_events << Course.past
     @past_events << Meeting.past
-    @past_events.flatten!.sort_by(&:date_and_time)
+    @past_events.flatten!.sort_by!(&:date_and_time).reverse!
 
     @events = Sessions.upcoming
     @events << Course.upcoming
     @events << Meeting.upcoming
-    @events.flatten!.sort_by(&:date_and_time)
+    @events.flatten!.sort_by!(&:date_and_time)
   end
 
 end
