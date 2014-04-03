dad9440a92b9bffcc7452de2264bc0596d3f2550
Despo Pentara <despo@extractmethod.com>
2014-04-03 03:51:25 +0100

    remove empty lines from address

--- app/decorators/address_decorator.rb
+++ app/decorators/address_decorator.rb
@@ -2,7 +2,7 @@ class AddressDecorator < Draper::Decorator
   delegate_all
 
   def to_html
-    [ flat, street, "London, #{postal_code}" ].join("<br/>").html_safe
+    [ flat, street, "London, #{postal_code}" ].compact.join("<br/>").html_safe
   end
 
 end
