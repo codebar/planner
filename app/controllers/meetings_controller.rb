68f532fda7823aa07aef14e40e9c8a72fa06bdb9
Despo Pentara <despo@extractmethod.com>
2014-04-17 15:06:14 +0100

    update meeting map

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -2,6 +2,7 @@ class MeetingsController < ApplicationController
 
   def show
     @meeting = Meeting.find(params[:id])
+    @map_address = AddressDecorator.new(@meeting.venue.address).for_map
   end
 end
 
