3be871cc7932c2f21b9396d1592095a642f7b8eb
Despo Pentara <despo@extractmethod.com>
2014-04-17 23:52:02 +0100

    upgrade to rails 4.1.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.4'
+gem 'rails', '4.1.0'
 
 gem 'haml'
 gem 'sass-rails', '~> 4.0.1'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,14 @@
 class EventsController < ApplicationController
 
   def index
-    @past_events = Sessions.past
-    @past_events << Course.past
-    @past_events << Meeting.past
+    @past_events = [ Sessions.past.all ]
+    @past_events << Course.past.all
+    @past_events << Meeting.past.all
     @past_events.flatten!.sort_by!(&:date_and_time).reverse!
 
-    @events = Sessions.upcoming
-    @events << Course.upcoming
-    @events << Meeting.upcoming
+    @events = [ Sessions.upcoming.all ]
+    @events << Course.upcoming.all
+    @events << Meeting.upcoming.all
     @events.flatten!.sort_by!(&:date_and_time)
   end
 end
