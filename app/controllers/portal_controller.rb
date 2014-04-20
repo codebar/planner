8fd4893806981eb0f1a150391407046a558f1149
Despo Pentara <despo@extractmethod.com>
2014-04-20 03:28:38 +0100

    update profile page and link to member portal

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -2,14 +2,5 @@ class PortalController < ApplicationController
   before_action :logged_in?
 
   def index
-    if current_member.is_student?
-      @upcoming_student = SessionInvitation.to_students.where(member: current_member).joins(:sessions).where("date_and_time >= ?", DateTime.now)
-      @attended = SessionInvitation.to_students.where(member: current_member).attended
-    end
-
-    if current_member.is_coach?
-      @upcoming_coach = SessionInvitation.to_coaches.where(member: current_member).joins(:sessions).where("date_and_time >= ?", DateTime.now)
-      @coached = SessionInvitation.to_coaches.where(member: current_member).attended
-    end
   end
 end
