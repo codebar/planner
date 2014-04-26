0da49b53b1a4f1a6e96317b36bc2acbfedcb5ceb
Despo Pentara <despo@extractmethod.com>
2014-04-26 01:23:09 +0100

    fix feedback form

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -6,19 +6,24 @@ class FeedbackController < ApplicationController
 
     set_coaches(feedback_request.sessions)
 
+    @session = feedback_request.sessions
     @feedback = Feedback.new
   end
 
   def submit
     if Feedback.submit_feedback(feedback_params, params[:id])
-      redirect_to success_feedback_path
+      flash[:notice] = I18n.t("messages.feedback_saved")
+
+      redirect_to root_path
     else
       feedback_request = FeedbackRequest.find_by(token: params[:id], submited: false)
       set_coaches(feedback_request.sessions)
 
+      @session = feedback_request.sessions
       @feedback = Feedback.new(feedback_params)
       @feedback.valid?
 
+      flash[:alert] = @feedback.errors.full_messages.to_sentence
       render 'show'
     end
   end
