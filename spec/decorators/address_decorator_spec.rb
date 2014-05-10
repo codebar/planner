beb1a23a3cbd2494800a697cfa55a2a4ba2981a3
Despo Pentara <despo@extractmethod.com>
2014-05-11 00:51:37 +0100

    update address spec to not use hardcoded city

--- spec/decorators/address_decorator_spec.rb
+++ spec/decorators/address_decorator_spec.rb
@@ -4,7 +4,7 @@ describe AddressDecorator do
   let(:address) { Fabricate.build(:address).decorate }
 
   it "#to_html" do
-    html_address = "#{address.flat}<br/>#{address.street}<br/>London, #{address.postal_code}"
+    html_address = "#{address.flat}<br/>#{address.street}<br/>#{address.city}, #{address.postal_code}"
 
     address.to_html.should eq(html_address)
   end
--- spec/fabricators/address_fabticator.rb
+++ spec/fabricators/address_fabticator.rb
@@ -2,4 +2,5 @@ Fabricator(:address) do
   flat { Faker::Address.street_name }
   street { Faker::Address.street_address }
   postal_code { Faker::Address.postcode }
+  city { Faker::Address.city }
 end
