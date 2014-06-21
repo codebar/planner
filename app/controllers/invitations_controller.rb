de1fed9cd756929ba4ef46462c63b0f910ec04f9
Despo Pentara <despo@extractmethod.com>
2014-06-21 23:56:47 +0100

    display invitations irrelevant of role

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,11 +4,9 @@ class InvitationsController < ApplicationController
   def index
     @upcoming_session = Sessions.next
 
-    @upcoming_student = SessionInvitation.to_students.where(member: current_user).joins(:sessions).where("date_and_time >= ?", DateTime.now)
-    @upcoming_student += CourseInvitation.where(member: current_user).joins(:course).where("date_and_time >= ?", DateTime.now)
-    @attended = SessionInvitation.to_students.where(member: current_user).attended
-    @upcoming_coach = SessionInvitation.to_coaches.where(member: current_user).joins(:sessions).where("date_and_time >= ?", DateTime.now)
-    @coached = SessionInvitation.to_coaches.where(member: current_user).attended
+    @upcoming_invitations = SessionInvitation.where(member: current_user).joins(:sessions).where("date_and_time >= ?", DateTime.now)
+    @upcoming_invitations += CourseInvitation.where(member: current_user).joins(:course).where("date_and_time >= ?", DateTime.now)
 
+    @attended_invitations = SessionInvitation.where(member: current_user).attended
   end
 end
