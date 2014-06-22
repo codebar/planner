d7f271d314630afd2cba7f46f1a99a9ba78513c8
Despo Pentara <despo@extractmethod.com>
2014-06-22 03:41:23 +0100

    show attendee avatars

--- app/models/concerns/invitable.rb
+++ app/models/concerns/invitable.rb
@@ -12,11 +12,11 @@ module Invitable
     end
 
     def attending_students
-      invitations.where(role: "Student").accepted
+      invitations.where(role: "Student").accepted.order('updated_at asc')
     end
 
     def attending_coaches
-      invitations.where(role: "Coach").accepted
+      invitations.where(role: "Coach").accepted.order('updated_at asc')
     end
 
   end
