bae9d8ad6fe6269a2821df4cc40304686a8b3880
Despo Pentara <despo@extractmethod.com>
2014-07-02 01:56:48 +0100

    select order by expressions for postgres

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -7,7 +7,7 @@ class Chapter < ActiveRecord::Base
   has_many :workshops, class_name: Sessions
   has_many :groups
   has_many :sponsors, through: :workshops
-  has_many :members, -> { order('subscriptions.created_at asc').uniq }, through: :groups
+  has_many :members, -> { select("subscriptions.created_at, members.*").order('subscriptions.created_at asc').uniq }, through: :groups
 
   def self.available_to_user(user)
     return Chapter.all if user.has_role?(:organiser) or user.has_role?(:admin) or user.has_role?(:organiser, Chapter)
