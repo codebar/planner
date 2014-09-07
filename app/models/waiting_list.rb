a81cab9a887ddc0fd0d99c159a7e016f4e6fb3b8
Despo Pentara <despo@extractmethod.com>
2014-09-07 02:22:30 +0100

    don’t use scope for next_spot as it returns relationship when there is no match

--- app/models/waiting_list.rb
+++ app/models/waiting_list.rb
@@ -3,11 +3,14 @@ class WaitingList < ActiveRecord::Base
   belongs_to :invitation, class: SessionInvitation
 
   scope :by_workshop, -> (workshop) { joins(:invitation).where("session_invitations.sessions_id = ?", workshop.id) }
-  scope :next_spot, -> (workshop, role) { by_workshop(workshop).where_role(role).where(auto_rsvp: true).first }
 
   scope :where_role, -> (role) { where("session_invitations.role = ?", role) }
 
   def self.add(invitation, auto_rsvp=true)
     create(invitation: invitation, auto_rsvp: auto_rsvp)
+  end
+
+  def self.next_spot(workshop, role)
+    by_workshop(workshop).where_role(role).where(auto_rsvp: true).first
   end
 end
