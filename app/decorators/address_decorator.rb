82eeb2282395ff40f46fdbe2f8eb6735bc650b45
Despo Pentara <despo@extractmethod.com>
2014-09-09 22:34:31 +0100

    remove empty strings

--- app/decorators/address_decorator.rb
+++ app/decorators/address_decorator.rb
@@ -2,14 +2,14 @@ class AddressDecorator < Draper::Decorator
   delegate_all
 
   def to_html
-    [ flat, street, "#{city}, #{postal_code}" ].compact.join("<br/>").html_safe
+    [ flat, street, "#{city}, #{postal_code}" ].delete_if(&:empty?).join("<br/>").html_safe
   end
 
   def for_map
-    [ flat, street, city, postal_code ].compact.join(",+")
+    [ flat, street, city, postal_code ].delete_if(&:empty?).join(",+")
   end
 
   def to_s
-    [ flat, street, city, postal_code ].compact.join(", ")
+    [ flat, street, city, postal_code ].delete_if(&:empty?).join(", ")
   end
 end
