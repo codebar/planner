1b4453c59fad063c9c336e39adc60b128264c101
Alex Pounds <alex+git@alexpounds.com>
2014-12-12 21:15:59 +0000

    A working method for attendance removal, and tests for it.

--- spec/features/workshops_spec.rb
+++ spec/features/workshops_spec.rb
@@ -46,7 +46,19 @@ feature 'Viewing a workshop page' do
     expect { click_button "Attend as a student"}.not_to change { ActionMailer::Base.deliveries.count }
   end
 
-  scenario "A logged-in user viewing a future event without student space can join the student waiting list"
+  scenario "A logged-in user viewing a future event without student space can join the student waiting list" do
+    workshop.host.update_attribute(:seats, 0)
+    login member
+    visit workshop_path workshop
+    expect(workshop.waitlisted? member).to be false
+    expect(page).not_to have_button("Attend as a student")
+    expect(page).to have_button("Join the student waiting list")
+
+    click_button "Join the student waiting list"
+    expect(current_path).to eq(waitlisted_workshop_path workshop)
+    expect(workshop.waitlisted? member).to be true
+    expect(workshop.attendee? member).to be false
+  end
 
   scenario "A logged-in user viewing a future event with coach space can register to attend" do
     login member
@@ -61,11 +73,68 @@ feature 'Viewing a workshop page' do
     expect(workshop.attending_coaches.map(&:member)).to include(member)
   end
 
-  scenario "A logged-in user viewing a future event without coach space can join the coach waiting list"
-  scenario "A logged-in user signed up on the student attendance list sees they are attending"
-  scenario "A logged-in user signed up on the student attendance list can remove themselves from the event"
-  scenario "A logged-in user signed up on the coach attendance list sees they are attending"
-  scenario "A logged-in user signed up on the coach attendance list can remove themselves from the event"
+  scenario "A logged-in user viewing a future event without coach space can join the coach waiting list" do
+    workshop.host.update_attribute(:seats, 0)
+    login member
+    visit workshop_path workshop
+    expect(workshop.waitlisted? member).to be false
+    expect(page).not_to have_button("Attend as a coach")
+    expect(page).to have_button("Join the coach waiting list")
+
+    click_button "Join the coach waiting list"
+    expect(current_path).to eq(waitlisted_workshop_path workshop)
+    expect(workshop.waitlisted? member).to be true
+    expect(workshop.attendee? member).to be false
+  end
+
+  scenario "A logged-in user signed up on the student attendance list sees they are attending" do
+    Fabricate(:student_session_invitation, attending: true, sessions: workshop, member: member)
+    expect(workshop.attendee? member).to be true
+
+    login member
+    visit workshop_path workshop
+    expect(page).not_to have_button("Attend as a student")
+    expect(page).not_to have_button("Join the student waiting list")
+    expect(page).to have_text("You're attending this event")
+    expect(page).to have_button("Cancel your attendance")
+  end
+
+  scenario "A logged-in user signed up on the student attendance list can remove themselves from the event" do
+    Fabricate(:student_session_invitation, attending: true, sessions: workshop, member: member)
+    expect(workshop.attendee? member).to be true
+    login member
+    visit workshop_path workshop
+    click_button "Cancel your attendance"
+
+    expect(current_path).to eq(removed_workshop_path workshop)
+    expect(workshop.attendee? member).to be false
+    expect(workshop.waitlisted? member).to be false
+  end
+
+  scenario "A logged-in user signed up on the coach attendance list sees they are attending" do
+    Fabricate(:coach_session_invitation, attending: true, sessions: workshop, member: member)
+    expect(workshop.attendee? member).to be true
+
+    login member
+    visit workshop_path workshop
+    expect(page).not_to have_button("Attend as a coach")
+    expect(page).not_to have_button("Join the coach waiting list")
+    expect(page).to have_text("You're attending this event")
+    expect(page).to have_button("Cancel your attendance")
+  end
+
+  scenario "A logged-in user signed up on the coach attendance list can remove themselves from the event" do
+    Fabricate(:coach_session_invitation, attending: true, sessions: workshop, member: member)
+    expect(workshop.attendee? member).to be true
+    login member
+    visit workshop_path workshop
+    click_button "Cancel your attendance"
+
+    expect(current_path).to eq(removed_workshop_path workshop)
+    expect(workshop.attendee? member).to be false
+    expect(workshop.waitlisted? member).to be false
+  end
+
   scenario "A logged-in user on the student waiting list sees that they're on the waiting list"
   scenario "A logged-in user on the student waiting list can see their waiting list position"
   scenario "A logged-in user on the student waiting list can remove themself from the waiting list"
