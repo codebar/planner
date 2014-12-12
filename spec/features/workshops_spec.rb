bf316fc284e6ce1d239fde74863dd1f70de58eac
Alex Pounds <alex+git@alexpounds.com>
2014-12-12 17:59:04 +0000

    More tests for student/coach registration from the web.

--- spec/features/workshops_spec.rb
+++ spec/features/workshops_spec.rb
@@ -36,6 +36,8 @@ feature 'Viewing a workshop page' do
     expect(page).to have_content("You're coming to Codebar!")
     expect(current_path).to eq(added_workshop_path workshop)
     expect(workshop.attendee? member).to be true
+    expect(workshop.attending_students.map(&:member)).to include(member)
+    expect(workshop.attending_coaches.map(&:member)).not_to include(member)
   end
 
   scenario "A logged-in user registering to attend an event doesn't receive an invite email" do
@@ -45,7 +47,20 @@ feature 'Viewing a workshop page' do
   end
 
   scenario "A logged-in user viewing a future event without student space can join the student waiting list"
-  scenario "A logged-in user viewing a future event with coach space can register to attend"
+
+  scenario "A logged-in user viewing a future event with coach space can register to attend" do
+    login member
+    visit workshop_path workshop
+    expect(workshop.attendee? member).to be false
+    click_button "Attend as a coach"
+
+    expect(page).to have_content("You're coming to Codebar!")
+    expect(current_path).to eq(added_workshop_path workshop)
+    expect(workshop.attendee? member).to be true
+    expect(workshop.attending_students.map(&:member)).not_to include(member)
+    expect(workshop.attending_coaches.map(&:member)).to include(member)
+  end
+
   scenario "A logged-in user viewing a future event without coach space can join the coach waiting list"
   scenario "A logged-in user signed up on the student attendance list sees they are attending"
   scenario "A logged-in user signed up on the student attendance list can remove themselves from the event"
