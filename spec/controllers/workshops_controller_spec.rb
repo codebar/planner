e02893c10a1170544f8ff11366642b14030bcbbf
Alex Pounds <alex+git@alexpounds.com>
2014-12-14 23:50:35 +0000

    Make sure ALL invites are removed when the user hits the 'remove' link.
    If a user had somehow got two invites (ie. as both a student AND a
    coach), the remove method would only attempt to un-attend one of them.
    This changeset ensures that we're removing both, and introduces a unit
    test for it too.

--- spec/controllers/workshops_controller_spec.rb
+++ spec/controllers/workshops_controller_spec.rb
@@ -45,4 +45,29 @@ describe WorkshopsController, type: :controller do
       expect(response).to redirect_to :workshop
     end
   end
+
+  describe "POST #remove" do
+    it "Redirects to the login page if the user's logged out" do
+      post :remove, id: workshop.id
+      expect(response).to redirect_to '/auth/github'
+    end
+
+    it "Does nothing if the student has no invite or waiting list" do
+      login member
+      post :remove, id: workshop.id
+      expect(response).to redirect_to removed_workshop_path(workshop)
+    end
+
+    it "Removes a student from the attendees, if the user's got an invite"
+    it "Removes a student from the waiting list, if the user's on the waiting list"
+    it "Removes both invites if the student has somehow got both a student and coach invite" do
+      Fabricate(:student_session_invitation, member: member, sessions: workshop, attending: true)
+      Fabricate(:coach_session_invitation, member: member, sessions: workshop, attending: true)
+      expect(workshop.attendee? member).to be true
+
+      login member
+      post :remove, id: workshop.id
+      expect(workshop.attendee? member).to be false
+    end
+  end
 end
