cd50211a7f9672e2ca6bfcfa0affa9e8980f5dde
Alex Pounds <alex+git@alexpounds.com>
2014-12-27 01:56:54 +0000

    Don't allow people to change their RSVPs close to the event time. If there's space at an event, they can still mark themselves as attending, and they can remove themselves/add themselves to the waiting list, as all of those things should be fine. Includes tests.

--- spec/features/workshops_spec.rb
+++ spec/features/workshops_spec.rb
@@ -38,6 +38,54 @@ feature 'Viewing a workshop page' do
     expect(page).to have_content("already happened")
   end
 
+  scenario "A logged-in user who's not attending can attend an imminent event" do
+    workshop.update_attribute(:date_and_time, Date.today)
+    workshop.update_attribute(:time, 2.hours.from_now)
+
+    login member
+    visit workshop_path workshop
+
+    expect(workshop.attendee? member).to be false
+    expect(page).to have_button("Attend as a student")
+    expect(page).to have_button("Attend as a coach")
+
+    click_button "Attend as a student"
+    expect(workshop.attendee? member).to be true
+    expect(page).to have_content("You're coming to Codebar!")
+    expect(current_path).to eq(added_workshop_path workshop)
+  end
+
+  scenario "A logged-in user who's attending an imminent event cannot interact with that event" do
+    workshop.update_attribute(:date_and_time, Date.today)
+    workshop.update_attribute(:time, 2.hours.from_now)
+    Fabricate(:student_session_invitation, attending: true, sessions: workshop, member: member)
+    expect(workshop.attendee? member).to be true
+
+    login member
+    visit workshop_path workshop
+
+    expect(page).not_to have_button("Attend as a student")
+    expect(page).not_to have_button("Attend as a coach")
+    expect(page).to have_content "If you can no longer make it, email us"
+  end
+
+  scenario "A logged-in user on the waiting list can remove themselves from the waiting list for an imminent event" do
+    workshop.update_attribute(:date_and_time, Date.today)
+    workshop.update_attribute(:time, 2.hours.from_now)
+    invite = Fabricate(:student_session_invitation, sessions: workshop, member: member)
+    WaitingList.add(invite)
+
+    login member
+    visit workshop_path workshop
+
+    expect(workshop.waitlisted? member).to be true
+
+    click_button "Leave the waiting list"
+    expect(current_path).to eq(removed_workshop_path workshop)
+    expect(workshop.waitlisted? member).to be false
+  end
+
+
   scenario "A logged-in user viewing a future event with student space can register to attend" do
     login member
     visit workshop_path workshop
