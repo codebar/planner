7466a14a692583ef70dd3d6d1d812c72828b6905
Alex Pounds <alex+git@alexpounds.com>
2015-01-11 16:20:38 +0000

    Ensure people can subscribe to mailing lists from step 2, and can't move past step 1 until they've provided all details.

--- spec/features/member_joining_spec.rb
+++ spec/features/member_joining_spec.rb
@@ -83,17 +83,64 @@ feature "A new student signs up", js: false do
     expect(member.about_you).to eq("I'm a test user created via the RSpec feature specs.")
   end
 
-  scenario "A student can sign up via the front page", pending: "Transform into test for edit page" do
-    visit root_path
-    click_on "Students"
-    expect(current_path).to eq(new_member_path)
+  scenario "Missing any detail on step 1 returns the user to step 1" do
+    member = Fabricate(:member, name: nil, surname: nil, email: nil, about_you: nil)
+    login member
+    member.update(can_log_in: true)
+    visit step1_member_path
 
-    click_on "Sign in with Github"
-    expect(current_path).to eq(edit_member_path)
-    expect(page).to have_selector("form")
-    fill_in 'member_about_you', with: "I'm a test user and am not actually real"
-    click_on "Save"
+    # Check for missing first name
+    fill_in "member_name", with: ""
+    fill_in "member_surname", with: "Smith"
+    fill_in "member_email", with: "someone@example.com"
+    fill_in "member_about_you", with: "Something"
+    click_on "Next"
+    expect(current_path).to eq(step1_member_path)
+    expect(page).to have_selector('.error')
 
-    expect(page).to have_text "Your details have been updated"
+    # Check for missing last name
+    fill_in "member_name", with: "Bob"
+    fill_in "member_surname", with: ""
+    fill_in "member_email", with: "someone@example.com"
+    fill_in "member_about_you", with: "Something"
+    click_on "Next"
+    expect(current_path).to eq(step1_member_path)
+    expect(page).to have_selector('.error')
+
+    # Check for missing email
+    fill_in "member_name", with: "Bob"
+    fill_in "member_surname", with: "Smith"
+    fill_in "member_email", with: ""
+    fill_in "member_about_you", with: "Something"
+    click_on "Next"
+    expect(current_path).to eq(step1_member_path)
+    expect(page).to have_selector('.error')
+
+    # Check for missing about you
+    fill_in "member_name", with: "Bob"
+    fill_in "member_surname", with: "Smith"
+    fill_in "member_email", with: "someone@example.com"
+    fill_in "member_about_you", with: ""
+    click_on "Next"
+    expect(current_path).to eq(step1_member_path)
+    expect(page).to have_selector('.error')
+  end
+
+  scenario "Picking a mailing list on step 2 subscribes you to that list" do
+    member = Fabricate(:member)
+    group = Fabricate(:group)
+    coach_group = Fabricate(:coaches)
+    expect(group.members.include? member).to be false
+    expect(coach_group.members.include? member).to be false
+
+    login member
+    member.update(can_log_in: true)
+    visit step2_member_path
+    expect(current_path).to eq(step2_member_path) # Make sure we didn't get redirected to step1 for missing details
+
+    expect(page).to have_selector('form')
+    click_button "Subscribe to #{group.chapter.name} student invites"
+    expect(group.members.include? member).to be true
+    expect(coach_group.members.include? member).to be false
   end
 end
