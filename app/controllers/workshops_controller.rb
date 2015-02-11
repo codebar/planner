a41faf658c9eb59fd7bdcfab783ff6310b56b87f
Despo Pentara <despo@extractmethod.com>
2015-02-11 07:40:17 +0000

    reuse invitation for managing attendance for now as workshop rsvp functionality is not fully implemented

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,11 +1,23 @@
 class WorkshopsController < ApplicationController
   before_action :authenticate_member!, except: [:show]
+  before_action :set_workshop, only: [:show, :rsvp]
 
   def show
-    @workshop = WorkshopPresenter.new(Sessions.find(params[:id]))
+    @workshop = WorkshopPresenter.new(@workshop)
     @host_address = AddressDecorator.decorate(@workshop.host.address) if @workshop.has_host?
   end
 
+  def rsvp
+    redirect_to :back, notice: "This workshop is not open for registrations" unless @workshop.invitable
+
+    if role_params.nil?
+      @invitation = SessionInvitation.where(sessions: @workshop, member: current_user, attending: true).first
+    else
+      @invitation = SessionInvitation.where(sessions: @workshop, member: current_user, role: role_params).first_or_create
+    end
+
+    redirect_to invitation_path(@invitation)
+  end
 
   # Add a user to this workshop, either to the attendees or the waiting list.
   def add
@@ -72,4 +84,14 @@ class WorkshopsController < ApplicationController
     @workshop = Sessions.find(params[:id])
     @coach = WaitingList.coaches(@workshop).include? current_user
   end
+
+  private
+
+   def set_workshop
+    @workshop = Sessions.find(params[:id])
+   end
+
+   def role_params
+     params[:role]
+   end
 end
