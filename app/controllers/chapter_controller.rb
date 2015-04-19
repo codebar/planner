c5002ae964fb21746586b7fbe2f2af8150ae773c
Despo Pentara <despo@despo.me>
2015-04-19 11:51:58 +0100

    a user can subscribe and unsubscribe from the chapter page

--- app/controllers/chapter_controller.rb
+++ app/controllers/chapter_controller.rb
@@ -8,7 +8,7 @@ class ChapterController < ApplicationController
 
     events = @chapter.upcoming_workshops.sort_by(&:date_and_time).group_by(&:date)
     @upcoming_workshops = events.map.inject({}) { |hash, (key, value)| hash[key] = EventPresenter.decorate_collection(value); hash}
-    past_events = [@chapter.workshops.most_recent].sort_by(&:date_and_time).group_by(&:date)
+    past_events = [@chapter.workshops.most_recent].compact.sort_by(&:date_and_time).group_by(&:date)
     @latest_workshops = past_events.map.inject({}) { |hash, (key, value)| hash[key] = EventPresenter.decorate_collection(value); hash}
   end
 
