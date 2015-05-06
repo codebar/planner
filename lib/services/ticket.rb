270a8cad3e028341f568bef475c37f94fe29aa4e
Despo Pentara <despo@despo.me>
2015-05-06 12:22:26 +0100

    Add web hook for rsvp’ing via tito

---
+++ lib/services/ticket.rb
@@ -0,0 +1,22 @@
+class Ticket
+
+  def initialize(request, params)
+    authorise(request)
+    @params = params
+  end
+
+  def email
+    @email ||= params[:email]
+  end
+
+  private
+
+  def params
+    @params
+  end
+
+  def authorise(request)
+    # TODO
+    # verify referer
+  end
+end
