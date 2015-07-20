4ce8dc090ce87209a4021a65bd3ba5d2fee6ca73
Denise <yu.denise.d@gmail.com>
2015-07-20 18:14:51 +0100

    added some basic tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,5 +5,14 @@ describe Event do
 
   it { should be_valid }
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:slug) }
+  it { should validate_presence_of(:info) }
+  it { should validate_presence_of(:schedule) }
+  it { should validate_presence_of(:description) }
+  it { should belong_to(:venue) }
+  it { should have_many(:sponsorships) }
+  it { should have_many(:sponsors) }
+  it { should have_many(:organisers) }
+  it { should have_and_belong_to_many(:chapters) }
 
 end
