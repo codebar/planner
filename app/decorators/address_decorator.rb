7e69c97c7fd9406367f51a3b4fc9a7e7276f4248
Denise <yu.denise.d@gmail.com>
2015-07-23 17:15:23 +0100

    added compact to remove nil fields

--- app/decorators/address_decorator.rb
+++ app/decorators/address_decorator.rb
@@ -2,7 +2,7 @@ class AddressDecorator < Draper::Decorator
   delegate_all
 
   def to_html
-    [ flat, street, "#{city}, #{postal_code}" ].delete_if(&:empty?).join("<br/>").html_safe
+    [ flat, street, "#{city}, #{postal_code}" ].compact.delete_if(&:empty?).join("<br/>").html_safe
   end
 
   def for_map
