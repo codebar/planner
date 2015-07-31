234e33e060ad742d6d9e268edb6b735ed5c370ff
Margo Urey <margo@margonline.co.uk>
2015-07-31 19:39:24 +0100

    correct the link to member

--- app/decorators/sponsor_decorator.rb
+++ app/decorators/sponsor_decorator.rb
@@ -2,12 +2,12 @@ class SponsorDecorator < Draper::Decorator
   delegate_all
 
   def contact_info
-    [ member_contact_details, contact_full_name, email ].flatten.delete_if(&:empty?).join("<br/>").html_safe
+    [ member_contact_details, contact_full_name, email ].flatten.compact.delete_if(&:empty?).join("<br/>").html_safe
   end
 
   def member_contact_details
     contacts.map do |member| 
-      h.link_to(member.full_name, member)
+      h.link_to(member.full_name, h.admin_member_path(member.id))
     end
   end
 
