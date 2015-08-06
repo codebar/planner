ce9c04732d36c975bbe2dcc74cd63dedf3c7efc7
Denise <yu.denise.d@gmail.com>
2015-08-06 14:40:34 -0700

    added ability to add/remove organisers
    removed validation on chapter for event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -8,6 +8,8 @@ class Admin::EventsController < Admin::ApplicationController
 
   def create
     @event = Event.new(event_params)
+    set_organisers(organiser_ids)
+
     if @event.save
       redirect_to [:admin, @event], notice: 'Event successfully created.'
     else
@@ -28,6 +30,8 @@ class Admin::EventsController < Admin::ApplicationController
   end
 
   def update
+    set_organisers(organiser_ids)
+
     if @event.update_attributes(event_params)
       redirect_to [:admin, @event], notice: "You have just updated the event"
     else
@@ -55,6 +59,26 @@ class Admin::EventsController < Admin::ApplicationController
 
   def event_params
     params.require(:event).permit(:name, :slug, :date_and_time, :begins_at, :ends_at, :description, :info, :schedule, :venue_id, :coach_spaces, :student_spaces, :email, :announce_only, :tito_url, :invitable, :student_questionnaire, :coach_questionnaire, sponsor_ids: [], chapter_ids: [])
+  end
+
+  def organiser_ids
+    params[:event][:organisers]
+  end
+
+  def grant_organiser_access(organiser_ids=[])
+    organiser_ids.each { |id| Member.find(id).add_role(:organiser, @event) }
+  end
+
+  def set_organisers(organiser_ids)
+    organiser_ids.reject!(&:empty?)
+    grant_organiser_access(organiser_ids)
+    revoke_organiser_access(organiser_ids)
+  end
+
+  def revoke_organiser_access(organiser_ids)
+    (@event.organisers.pluck(:id).map(&:to_s) - organiser_ids).each do |id|
+      Member.find(id).revoke(:organiser, @event)
+    end
   end
 
   def find_event
