ef169197e93a888fef31e16254478e11e2366d16
Denise <yu.denise.d@gmail.com>
2015-08-08 18:14:08 -0700

    added redirect for when member tries to RSVP to two events at same time

--- spec/features/coach_accepting_invitation_spec.rb
+++ spec/features/coach_accepting_invitation_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 feature 'a Coach can' do
 
   context "#session" do
+    let(:member) { Fabricate(:member) }
     let(:invitation) { Fabricate(:coach_session_invitation) }
     let(:invitation_route) { invitation_path(invitation) }
 
     let(:set_no_available_slots) { invitation.sessions.host.update_attribute(:seats, 0) }
+
+    before(:each) do
+      login(member)
+    end
 
     it_behaves_like "invitation route"
 
