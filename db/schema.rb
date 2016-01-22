4e19d6d6ca0db721dab9198a77f1f16619703464
Endi Sukaj <endisukaj@gmail.com>
2016-01-22 16:49:57 +0100

    Added fix for issue #378 Only display skills label if coach has skills set

--- db/schema.rb
+++ db/schema.rb
@@ -329,7 +329,6 @@ ActiveRecord::Schema.define(version: 20151126102454) do
     t.string   "mobile"
     t.boolean  "received_coach_welcome_email",   default: false
     t.boolean  "received_student_welcome_email", default: false
-    t.boolean  "confirmed_eligibility"
   end
 
   create_table "members_permissions", id: false, force: true do |t|
