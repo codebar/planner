769897e79a51e959f13e35e71da9bf7214bc6d3f
Denise Yu <yu.denise.d@gmail.com>
2016-02-08 18:47:53 +0000

    add map to meeting page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -5,6 +5,7 @@ class MeetingsController < ApplicationController
     if @meeting.attending?(current_user)
       @invitation = MeetingInvitation.where(meeting: @meeting, member: current_user).last
     end
+    @host_address = AddressDecorator.decorate(@meeting.venue.address)
   end
 
   private
