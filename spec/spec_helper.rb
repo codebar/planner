45f94956885cde517290216170fb00abecfe3d50
Despo Pentara <despo@despo.me>
2016-03-04 18:59:25 +0000

    add codeclimate-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'simplecov'
 require 'coveralls'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
