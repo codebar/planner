d09adfde5c97b37f34b0db5a44b2f1083902e68e
Despo Pentara <despo@despo.me>
2016-03-08 00:41:52 +0000

    remove obsolete views/controller code and tests. add feature tests for actual states of workshop#show

--- spec/features/workshops_spec.rb
+++ spec/features/workshops_spec.rb
@@ -26,6 +26,24 @@ feature 'Viewing a workshop page' do
       visit workshop_path(workshop)
     end
 
+    context "#upcoming workshop" do
+      it "can get to the student and coach invitation pages" do
+
+        expect(page).to have_link("Attend as a student")
+        expect(page).to have_link("Attend as a coach")
+      end
+
+      context "when invitations have been sent out" do
+        let!(:invitation) { Fabricate(:attending_session_invitation, member: member, workshop: workshop) }
+
+        it "can manage vie details if they are already attending" do
+          visit workshop_path(workshop)
+
+          expect(page).to have_link("Manage your invitation")
+        end
+      end
+    end
+
     context "#past events" do
       let!(:workshop) { Fabricate(:workshop, date_and_time: 2.weeks.ago) }
 
@@ -34,6 +52,5 @@ feature 'Viewing a workshop page' do
         expect(page).to have_content("has already taken place")
       end
     end
-
   end
 end
