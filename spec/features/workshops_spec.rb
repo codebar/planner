11bd7501b31c9d25d0f56ddc9c63110ec5dc5160
Despo Pentara <despo@despo.me>
2016-03-08 01:12:29 +0000

    improve workshop page tests

--- spec/features/workshops_spec.rb
+++ spec/features/workshops_spec.rb
@@ -27,10 +27,25 @@ feature 'Viewing a workshop page' do
     end
 
     context "#upcoming workshop" do
-      it "can get to the student and coach invitation pages" do
+      context "when invitations have not been sent out" do
+        it "can access RSVP as a student" do
+          click_on "Attend as a student"
+          click_on "Attend"
 
-        expect(page).to have_link("Attend as a student")
-        expect(page).to have_link("Attend as a coach")
+          expect(page).to have_content("See you at the workshop")
+        end
+
+        it "can access RSVP as a coach" do
+          click_on "Attend as a coach"
+          click_on "Attend"
+
+          expect(page).to have_content("See you at the workshop")
+        end
+
+        after do
+          visit workshop_path(workshop)
+          expect(page).to have_link("Manage your invitation")
+        end
       end
 
       context "when invitations have been sent out" do
