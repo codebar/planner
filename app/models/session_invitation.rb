d64364727aed7accf3ff96fa0cf32b4535c3c77e
Denise Yu <yu.denise.d@gmail.com>
2016-03-24 17:46:57 +0000

    Add flag on admin workshop view for members with more than 2 past absences

--- app/models/session_invitation.rb
+++ app/models/session_invitation.rb
@@ -8,11 +8,13 @@ class SessionInvitation < ActiveRecord::Base
   validates :member_id, uniqueness: { scope: [:workshop_id, :role ] }
   validates_inclusion_of :role, in: [ "Student", "Coach" ], allow_nil: true
 
+  scope :accepted, -> { where(attending: true) }
   scope :attended, -> { where(attended: true) }
   scope :to_students, -> { where(role: "Student") }
   scope :to_coaches, -> { where(role: "Coach") }
   scope :by_member, -> { group(:member_id) }
   scope :order_by_latest, -> { joins(:workshop).order('workshops.date_and_time desc') }
+  scope :past, -> { joins(:workshop).where('workshops.date_and_time < ?', Date.today) }
 
   def waiting_list_position
     @waiting_list_position ||= WaitingList.by_workshop(self.workshop).where_role(self.role).where(auto_rsvp: true).order(:created_at).map(&:invitation_id).index(self.id)+1
