5ca1756fb3b9f648009cde7a527e408d2722b171
Denise Yu <yu.denise.d@gmail.com>
2016-04-13 20:49:08 +0100

    omg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Planner::Application.configure do
     :port =>           '587',
     :address =>        'smtp.sendgrid.net',
     :user_name =>      ENV['SENDGRID_USERNAME'],
-    :password =>       ENV['SENDGRID_APIKEY'],
+    :password =>       ENV['SENDGRID_PASSWORD'],
     :domain =>         'heroku.com',
     :authentication => :plain,
     :enable_starttls_auto => true
