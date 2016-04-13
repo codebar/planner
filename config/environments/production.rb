749cd599c1d911a8d23ee53b1c13fe0fdc0efe1f
Denise Yu <yu.denise.d@gmail.com>
2016-04-13 20:39:09 +0100

    add more credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,8 @@ Planner::Application.configure do
     :user_name =>      ENV['SENDGRID_USERNAME'],
     :password =>       ENV['SENDGRID_APIKEY'],
     :domain =>         'codebar.io',
-    :authentication => :plain
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 
