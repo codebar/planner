d55ab700f2b4c33e0287eb78ccd10e401504824a
Denise Yu <yu.denise.d@gmail.com>
2016-04-13 20:46:26 +0100

    try setting domain to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Planner::Application.configure do
     :address =>        'smtp.sendgrid.net',
     :user_name =>      ENV['SENDGRID_USERNAME'],
     :password =>       ENV['SENDGRID_APIKEY'],
-    :domain =>         'codebar.io',
+    :domain =>         'heroku.com',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
