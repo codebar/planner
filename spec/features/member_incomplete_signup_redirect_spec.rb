141bda1edaf3fc533276e7c7331eb0564f8c8326
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-07 13:13:05 -0700

    Complete test for new feature

--- spec/features/member_incomplete_signup_redirect_spec.rb
+++ spec/features/member_incomplete_signup_redirect_spec.rb
@@ -15,5 +15,14 @@ feature 'A new member signs up', js: false do
 
   scenario 'Member visits tutorial page before completing step 1 of registration' do
     member = Fabricate(:member)
+    member.update(can_log_in: :true)
+    login member
+    visit step1_member_path
+
+    within('.top-bar') do
+      click_on 'Tutorials'
+    end
+
+    expect(current_path).to eq(step1_member_path)
   end
 end
