6616f7b0bc6275ec99c317c8dd4d45ecd22698a2
Donna Zhou <donnasiqizhou@gmail.com>
2017-03-23 14:25:50 +0100

    Re-added past scope for dependencies

--- app/models/concerns/listable.rb
+++ app/models/concerns/listable.rb
@@ -5,9 +5,9 @@ module Listable
   extend ActiveSupport::Concern
 
   included do
-
     scope :upcoming, -> { where("date_and_time >= ?", Time.zone.now).order(:date_and_time) }
-    scope :past_display, -> { where("date_and_time < ?", Time.zone.now).order(date_and_time: :desc).includes(:chapter).limit(NUMBER_OF_PAST_EVENTS_FOR_INDEX) }
+    scope :past, -> { where("date_and_time < ?", Time.zone.now).order(:date_and_time) }
+    scope :past_display, -> { where("date_and_time < ?", Time.zone.now).order(date_and_time: :desc).limit(NUMBER_OF_PAST_EVENTS_FOR_INDEX) }
     scope :past_count, -> { where("date_and_time < ?", Time.zone.now).count}
     scope :recent, -> { where("date_and_time < ?", Time.zone.now).order(date_and_time: :desc).limit(NUMBER_OF_RECENT_WORKSHOPS_TO_RETRIEVE) }
   end
