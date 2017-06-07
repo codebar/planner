60af89784e4ad5d68e5683a8e2090a5226df302b
Ashley Schauer <aelschauer@gmail.com>
2017-06-06 20:28:44 -0600

    add redirect to feature test

--- spec/support/helpers/login_helpers.rb
+++ spec/support/helpers/login_helpers.rb
@@ -3,7 +3,7 @@ module LoginHelpers
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(member)
   end
 
-  def login_mock_omniauth(member)
+  def login_mock_omniauth(member, login_link = 'Sign in')
     OmniAuth.config.mock_auth[:github] = {
       provider: member.auth_services.first.provider,
       uid: member.auth_services.first.uid,
@@ -12,7 +12,7 @@ module LoginHelpers
         secret: 'mock_secret'
       }
     }
-    click_on 'Sign in'
+    click_on login_link
   end
 
   def login_as_admin(member)
