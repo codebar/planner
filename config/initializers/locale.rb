eb47f3d44e331bdeab1358a111840cb3fcc3c5d4
matyikriszta <matyi.kriszta@gmail.com>
2017-12-11 12:22:53 +0100

    Moves I18n module configuration from config/application.rb to an initializer

---
+++ config/initializers/locale.rb
@@ -0,0 +1,6 @@
+# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+config.i18n.default_locale = :en
+config.i18n.fallbacks = true
+config.i18n.available_locales = [:en, :fr]
+#config.i18n.enforce_available_locales = false
