89ed68d62cecbdbccda115c31ab578f80e9a6460
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-15 15:28:33 +0000

    footer page in progress

--- ui_testing/features/lib/codebar.rb
+++ ui_testing/features/lib/codebar.rb
@@ -1,5 +1,15 @@
+require_relative '../lib/soraia_pages/footer'
+require_relative '../lib/soraia_pages/tutorials_page'
+
 
 module CodeBar
 
+  def footer
+    CodeBarFooter.new
+  end
+
+  def tutorials_page
+    TutorialsPage.new
+  end
 
 end
--- ui_testing/features/lib/pages/soraia_pages/footer.rb
+++ ui_testing/features/lib/pages/soraia_pages/footer.rb
@@ -1,1 +1,31 @@
+require 'capybara'
 
+class CodeBarFooter
+  include Capybara::DSL
+
+  CODE_OF_CONDUCT_URL = '/code-of-conduct'
+  COACH_GUIDE_URL = '/effective-teacher-guide'
+  STUDENT_GUIDE_URL = '/student-guide'
+  TUTORIALS_URL = 'http://tutorials.codebar.io'
+  BECOMING_SPONSOR_URL = 'http://manual.codebar.io/becoming-a-sponsor'
+  FAQ_URL = '/faq'
+  BLOG_URL = 'https://medium.com/the-codelog'
+  COACHES_URL = '/coaches'
+  SPONSORS_URL = '/sponsors'
+  EVENTS_URL = '/events'
+  JOBS_URL = '/jobs'
+  DONATE_URL = '/donations/new'
+  SLACK_URL = 'https'
+  GITHUB_URL =
+  TWITTER_URL =
+  FACEBOOK_URL =
+
+  def visit_home_page
+    visit("/")
+  end
+
+  def visit_code_of_conduct
+
+  end
+
+end
