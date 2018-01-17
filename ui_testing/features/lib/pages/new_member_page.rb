8fec725513a0a04c7358d238cad3626acf4a1f0f
Tech-A42 <Tech-A42@Tech-A42s-MacBook-Air.local>
2018-01-17 14:13:28 +0000

    Constants added;

---
+++ ui_testing/features/lib/pages/new_member_page.rb
@@ -0,0 +1,14 @@
+require 'capybara/dsl'
+
+class NewMember
+  include Capybara::DSL
+
+  def click_sign_up_student
+
+  end
+  
+  def click_sign_up_coach
+
+  end
+
+end
---
+++ ui_testing/features/step_definitions/sign_up_step_defs.rb
@@ -0,0 +1,27 @@
+When("I sign up as a student") do
+  homepage.click_sign_up_student
+end
+
+And("I accept criterias") do
+
+end
+
+And("I submit the details") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will have signed up") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I sign up as a coach") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I host a workshop") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I get redirected to the email app") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
