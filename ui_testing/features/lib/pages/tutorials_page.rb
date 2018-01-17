72fc385e26c50648d2b2eeb21a8a4e55764e9f29
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-17 09:55:23 +0000

    some tests written for tutorials page

--- ui_testing/features/lib/pages/tutorials_page.rb
+++ ui_testing/features/lib/pages/tutorials_page.rb
@@ -1,9 +1,21 @@
-require 'capybara'
-
-class TutorialsPage
-  include Capybara::DSL
-
-  
-
-
-end
+# require 'capybara/dsl'
+#
+# class TutorialsPage
+#   include Capybara::DSL
+#
+#   CODE_OF_CONDUCT = 'Code of conduct'
+#   COACH_GUIDE = 'Coach guide'
+#   STUDENT_GUIDE = 'Student guide'
+#   BECOMING_SPONSOR_URL = 'Becoming a sponsor'
+#   FAQ = 'FAQ'
+#
+#   def visit_home_page
+#     visit("/")
+#   end
+#
+#   def visit_code_of_conduct
+#     find_link(CODE_OF_CONDUCT)
+#   end
+#
+#
+# end
