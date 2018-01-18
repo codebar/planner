2dade33d47131e0c6c218a1bb4212de0aa226a39
solufemi91 <stevenoo@spartaglobal.com>
2018-01-18 22:48:55 +0000

    cleared test ste defs file

--- UI-test/features/step_definitions/test_step_defs.rb
+++ UI-test/features/step_definitions/test_step_defs.rb
@@ -1,23 +1,5 @@
 Given("I am on a page") do
-  homepage.visit_home_page
-
-  sign_in.sign_in_link
-
-  sign_in.enter_github_username
-
-  sign_in.enter_github_psswrd
-
-  sign_in.confirm_github_details
-
-  invitations_page.visit_invitations_page
-  sleep 4
-  invitations_page.click_attended
-  sleep 4
-  # sleep 4
-  # events_someslug_page.click_rsvp_coach
-  # sleep 4
-  # events_someslug_page.find_alert_box_attending
-  # sleep 4
+  pending
 end
 
 When("I click link") do
