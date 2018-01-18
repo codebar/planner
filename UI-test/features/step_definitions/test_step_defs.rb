f352a4c62e76fc416792b5a4d181985c39cac311
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-18 15:10:28 +0000

    cucumber test step defs functional, jon features removed

--- UI-test/features/step_definitions/test_step_defs.rb
+++ UI-test/features/step_definitions/test_step_defs.rb
@@ -1,13 +1,12 @@
+
 Given("I am on a page") do
-  pending
+  footer.visit_homepage
 end
 
 When("I click link") do
   footer.click_chosen_link('Code of conduct')
-
 end
 
 Then("It works") do
   expect( title_search.title_check ).to eq 'Code of conduct'
-
 end
--- UI-test/features/test.feature
+++
@@ -1,7 +0,0 @@
-Feature: Test
-
-  Scenario: Test
-
-    Given I am on a page
-    When I click link
-    Then It works
