3e0c3a599cdf1f23c59ef6f6a68fce46797bacbf
solufemi91 <stevenoo@spartaglobal.com>
2018-01-18 15:59:03 +0000

    cleared the stepdefs

--- UI-test/features/step_definitions/test_step_defs.rb
+++ UI-test/features/step_definitions/test_step_defs.rb
@@ -1,10 +1,5 @@
 Given("I am on a page") do
-  events_page.visit_events_page
-  # sleep 4
-  # events_page.click_events_link
-  # sleep 4
-  events_page.find_sponsors_images
-  sleep 4
+  pending
 end
 
 When("I click link") do
