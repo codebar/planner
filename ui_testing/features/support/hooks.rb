78d54830a6b073b832b6f1dad18032bbfdd6a328
Tech-A42 <Tech-A42@Tech-A42s-MacBook-Air.local>
2018-01-19 10:06:39 +0000

    signout of github scenario removed

--- ui_testing/features/support/hooks.rb
+++ ui_testing/features/support/hooks.rb
@@ -3,3 +3,9 @@ After ("@sign-up-student") do
   github.click_profile_icon
   github.click_sign_out
 end
+
+After ("@sub_student") do
+  github.visit_github
+  github.click_profile_icon
+  github.click_sign_out
+end
