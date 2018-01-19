64e6b84561cb8df44390ae2bf140ca04e7071f54
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-19 10:54:27 +0000

    page functions sorted alphabetically

--- UI-test/features/lib/codebar_site.rb
+++ UI-test/features/lib/codebar_site.rb
@@ -1,47 +1,47 @@
+# pages
+require_relative './pages/coaches_page'
+require_relative './pages/code_of_conduct_page'
+require_relative './pages/dashboard_page'
+require_relative './pages/events_page'
+require_relative './pages/events_someslug_page'
+require_relative './pages/github_logout_page'
+require_relative './pages/homepage'
+require_relative './pages/invitations_page'
+require_relative './pages/new_job_page'
+require_relative './pages/sign_in_page'
+require_relative './pages/sponsors_page'
+require_relative './pages/student_guide_page'
+require_relative './pages/subscriptions_page'
+require_relative './pages/tutorials_index_page'
+require_relative './pages/tutorials_page'
+require_relative './pages/update_profile_page'
+# partials
 require_relative './partials/navbar_partial'
 require_relative './partials/footer_partial'
 require_relative './partials/title_search'
-require_relative './pages/homepage'
-require_relative './pages/code_of_conduct_page'
-require_relative './pages/student_guide_page'
-require_relative './pages/tutorials_page'
-require_relative './pages/tutorials_index_page'
-require_relative './pages/coaches_page'
-require_relative './pages/sponsors_page'
-require_relative './pages/events_page'
-require_relative './pages/dashboard_page'
-require_relative './pages/events_someslug_page'
-require_relative './pages/invitations_page'
-require_relative './pages/subscriptions_page'
-require_relative './pages/new_job_page'
-require_relative './pages/sign_in_page'
-require_relative './pages/github_logout_page'
 
-# require_relative './profile_info/signin_page'
-# require_relative './profile_info/update_profile'
-# require_relative './jobs/post_a_job'
-# require_relative './tutorials/tutorials_pages'
 
 module CodebarSite
-
-  def navbar
-    NavbarPartial.new
+  
+  # pages
+  def coaches_page
+    CoachesPage.new
   end
 
-  def footer
-    FooterPartial.new
+  def code_of_conduct_page
+    CodeOfConductPage.new
   end
 
-  def title_search
-    TitleSearch.new
+  def dashboard_page
+    DashBoardPage.new
   end
 
-  def sign_in_page
-    SignInPage.new
+  def events_page
+    EventsPage.new
   end
 
-  def update_profile
-    UpdateProfile.new
+  def events_someslug_page
+    EventsSomeSlugPage.new
   end
 
   def github_logout_page
@@ -52,8 +52,20 @@ module CodebarSite
     HomePage.new
   end
 
-  def code_of_conduct_page
-    CodeOfConductPage.new
+  def invitations_page
+    InvitationsPage.new
+  end
+
+  def new_job_page
+    NewJobPage.new
+  end
+
+  def sign_in_page
+    SignInPage.new
+  end
+
+  def sponsors_page
+    SponsorsPage.new
   end
 
   def student_guide_page
@@ -64,40 +76,29 @@ module CodebarSite
     TutorialsIndexPage.new
   end
 
-  def new_job_page
-    NewJobPage.new
-  end
-
-  def coaches_page
-    CoachesPage.new
-  end
-
-  def sponsors_page
-    SponsorsPage.new
-  end
-
-  def events_page
-    EventsPage.new
-  end
-
-  def dashboard_page
-    DashBoardPage.new
-  end
-
-  def events_someslug_page
-    EventsSomeSlugPage.new
-  end
-
-  def invitations_page
-    InvitationsPage.new
+  def tutorials_page
+    TutorialsPage.new
   end
 
   def subscriptions_page
     SubscriptionsPage.new
   end
 
-  def tutorials_page
-    TutorialsPage.new
+  def update_profile_page
+    UpdateProfilePage.new
+  end
+
+  # partials
+  def navbar
+    NavbarPartial.new
+  end
+
+  def footer
+    FooterPartial.new
+  end
+
+  def title_search
+    TitleSearch.new
   end
 
 end
