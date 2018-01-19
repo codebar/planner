75ef36bc6f0bc8c10982cbd4a12b3bfca98ba944
solufemi91 <stevenoo@spartaglobal.com>
2018-01-19 10:53:42 +0000

    added a profile POM

--- UI-test/features/lib/pages/profile_page.rb
+++ UI-test/features/lib/pages/profile_page.rb
@@ -4,7 +4,8 @@ class ProfilePage
   include Capybara::DSL
 
   VISIT_PROFILE_PAGE = 'http://localhost:3000/profile' unless const_defined?(:VISIT_PROFILE_PAGE)
-  PROFILE_TITLE = 'My Profile'
+  PROFILE_TITLE = 'My Profile' unless const_defined?(:PROFILE_TITLE)
+  UPDATE_DETAILS_LINK = 'Update your details' unless const_defined?(:UPDATE_DETAILS_LINK)
 
   def visit_profile_page
     visit(VISIT_PROFILE_PAGE)
@@ -12,6 +13,10 @@ class ProfilePage
 
   def find_profile_title
     page.find('h2', text: PROFILE_TITLE)
+  end
+
+  def click_update_details
+    click_link(UPDATE_DETAILS_LINK)
   end
 
 
--- UI-test/features/step_definitions/test_step_defs.rb
+++ UI-test/features/step_definitions/test_step_defs.rb
@@ -1,12 +1,5 @@
 Given("I am on a page") do
-  sign_in.visit_homepage
-  sign_in.sign_in_link
-  sign_in.enter_github_username
-  sign_in.enter_github_psswrd
-  sign_in.confirm_github_details
-  profile_page.visit_profile_page
-  profile_page.find_profile_title
-  sleep 10
+  pending
 end
 
 When("I click link") do
