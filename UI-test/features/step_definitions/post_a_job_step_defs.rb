afbeeefe253c47e514e850e7ee79e40216968ab8
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2018-01-19 16:08:54 +0000

    post a job step defs added

---
+++ UI-test/features/step_definitions/post_a_job_step_defs.rb
@@ -0,0 +1,23 @@
+Given("I am logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on the list a job option in the menu") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be sent to a page where I can enter the deatils of the job") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And("I enter all the required fields for the job and submit") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And("I should be taken to a new page to confirm") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And("I should be notifitief of my submission for a job posting") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
