88737637235f73b80272eff0867276aeb980c8a2
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-22 10:56:03 +0000

    error with regex corrected

--- UI-test/features/step_definitions/footer_navigation_step_defs.rb
+++ UI-test/features/step_definitions/footer_navigation_step_defs.rb
@@ -3,11 +3,11 @@ Given("I am on the homepage") do
   homepage.visit_home_page
 end
 
-When(/^I click on a link in the footer(.*)$/) do |link|
-  footer.click_footer_link(link[1..-1])
+When(/^I click on a link in the footer (.*)$/) do |link|
+  footer.click_footer_link(link)
 end
 
-Then(/^I am sent to a page with the correct title(.*)$/) do |title|
-  expect(title_search.title_check).to eq(title[1..-1])
+Then(/^I am sent to a page with the correct title (.*)$/) do |title|
+  expect(title_search.title_check).to eq(title)
 
 end
