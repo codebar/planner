16d64f343311a4b83c39f255cae507861959b072
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-22 16:12:44 +0000

    tests for returning to homepage after accessing a tutorial

--- UI-test/features/step_definitions/access_tutorial_step_defs.rb
+++ UI-test/features/step_definitions/access_tutorial_step_defs.rb
@@ -11,3 +11,8 @@ Then("I can return to the previous page with a link") do
   tutorials_page.go_back
   expect(current_url).to eq('http://tutorials.codebar.io/')
 end
+
+Then("I can return to the home page with a link") do
+  tutorials_page.go_home
+  expect(current_url).to eq('https://codebar.io/')
+end
