403776a3d2659bab891dcadfa6b1e2ec6dea3fea
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-23 16:53:14 +0000

    failing test moved to end other failed test file renamed so wont run

--- UI-test/features/step_definitions/manage_subscriptions_sidebar_step_defs.rb
+++ UI-test/features/step_definitions/manage_subscriptions_sidebar_step_defs.rb
@@ -9,23 +9,19 @@ end
 
 And("I have clicked managed subscriptions") do
   navbar.click_aside_manage_subscriptions
-  sleep 3
 end
 
 And("I have been redirected to the subscriptions page") do
   url = URI.parse(current_url)
   expect(url).to eq URI.parse(subscriptions_page.subscriptions_page_link)
-  sleep 4
 end
 
 When("I click to subscribe to a particuar region") do
   subscriptions_page.select_sub
-  sleep 4
 end
 
 Then("I should be notified that I have succesfully subscribed for a particular region") do
   expect(subscriptions_page.confirm_subs_updated).to eq true
-  sleep 4
   subscriptions_page.select_sub
   sign_in_page.sign_out_func
   github_logout_page.github_logout_func
