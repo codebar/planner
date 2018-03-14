a03602caa078050156f0ae138b01fda41ff7561c
Adam Howard <skattyadz+gh@gmail.com>
2018-03-14 15:39:44 -0700

    Add wall_of_fame action test to bump coverage

---
+++ spec/features/listing_coaches_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'when visiting the coaches page' do
+  let!(:coach) { Fabricate(:coach_session_invitation, attended: true).member }
+
+  scenario 'I can see the most active coaches' do
+    visit coaches_path
+    expect(page).to have_content coach.name
+  end
+end
