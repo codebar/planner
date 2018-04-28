b60305c3091de76b04a44af1a0c3de4ae7830bd7
Despo Pentara <despo@extractmethod.com>
2018-04-28 02:25:41 +0100

    fix skills page falling over

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,11 @@
 class SkillsController < ApplicationController
   def show
     @skill = params[:id]
-    @coaches = Member.with_skill(params[:id])
+    @coaches = Member.with_skill(params[:id]).paginate(page: page)
+  end
+
+  private
+  def page
+    params.permit(:page)[:page]
   end
 end
