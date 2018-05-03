9b49c9375c04bdbb7665d1be60c97e12fdaed616
Despo Pentara <despo@extractmethod.com>
2018-05-03 01:51:39 +0100

    ensure chapter is present before performing date validation (As It causes app to crash since timezone is on chapter)
    - `includes(:member)` on attendances as per Buller error
    - Restrict access to non valid route /admin/workshops
    - improve test coverage

--- app/presenters/workshop_presenter.rb
+++ app/presenters/workshop_presenter.rb
@@ -34,7 +34,7 @@ class WorkshopPresenter < EventPresenter
   end
 
   def attendees_emails
-    model.attendances.map { |m| m.member.email if m.member }.compact.join(', ')
+    model.attendances.includes(:member).map { |m| m.member.email if m.member }.compact.join(', ')
   end
 
   def time
--- spec/features/admin/workshops_spec.rb
+++ spec/features/admin/workshops_spec.rb
@@ -10,16 +10,39 @@ feature 'Managing workshops' do
     member.add_role(:organiser, Chapter)
   end
 
-  scenario 'creating a new workshop' do
-    visit new_admin_workshop_path
+  scenario 'viewing list of all chapter workshops' do
+    workshops = Fabricate.times(5, :workshop, chapter: chapter)
+    visit admin_chapter_workshops_path(chapter)
 
-    select chapter.name
-    fill_in 'Local date', with: Date.current
-    fill_in 'Local time', with: '11:30'
+    workshops.each do |workshop|
+      expect(page).to have_content(humanize_date(workshop.date_and_time, with_time: true))
+    end
+  end
 
-    click_on 'Save'
+  context 'creating a new worksbop' do
+    scenario 'successfuly' do
+      visit new_admin_workshop_path
 
-    expect(page).to have_content 'Invite'
+      select chapter.name
+      fill_in 'Local date', with: Date.current
+      fill_in 'Local time', with: '11:30'
+
+      click_on 'Save'
+
+      expect(page).to have_content('The workshop has been created')
+      expect(page).to have_content 'Invite'
+    end
+
+    scenario 'renders an error when no chapter has been selected' do
+      visit new_admin_workshop_path
+
+      fill_in 'Local date', with: Date.current
+      fill_in 'Local time', with: '11:30'
+
+      click_on 'Save'
+
+      expect(page).to have_content('Chapter can\'t be blank')
+    end
   end
 
   scenario 'assigning a host to a workshop' do
@@ -46,5 +69,38 @@ feature 'Managing workshops' do
     within '#sponsors' do
       expect(page).to have_content sponsor.name
     end
+  end
+
+  scenario  'sending invitations to workshop attendees' do
+    workshop = Fabricate(:workshop)
+    visit admin_workshop_send_invites_path(workshop)
+    click_on 'Students'
+
+    expect(page).to have_content('Invitations to students are being emailed out')
+  end
+
+  scenario 'rendering a text file with all the workshop attendee emails' do
+    workshop = Fabricate(:workshop)
+    attendees = Fabricate.times(4, :attending_workshop_invitation, workshop: workshop)
+    attendee_emails = attendees.map(&:member).map(&:email).join(', ')
+    visit admin_workshop_attendees_emails_path(workshop, format: :text)
+
+    expect(page).to have_content(attendee_emails)
+  end
+
+  scenario 'rendering a list with the workshop attendee names' do
+    workshop = Fabricate(:workshop)
+    attendees = Fabricate.times(4, :attending_workshop_invitation, workshop: workshop)
+    visit admin_workshop_attendees_checklist_path(workshop, format: :text)
+    attendee_emails = attendees.map(&:member).map(&:full_name).each do |name|
+      expect(page).to have_content(name)
+    end
+  end
+
+  scenario 'attempting to render a list with the workshop attendee names without a request text format' do
+    workshop = Fabricate(:workshop)
+    visit admin_workshop_attendees_checklist_path(workshop)
+
+    expect(page.current_path).to eq(admin_workshop_path(workshop))
   end
 end
