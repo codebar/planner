055531d75de1d386f9f9ee867a601326a872a1f6
Despo Pentara <despo@extractmethod.com>
2018-05-03 02:50:56 +0100

    add tests for invitation_presenter#attendance_status

--- spec/presenters/invitation_presenter_spec.rb
+++ spec/presenters/invitation_presenter_spec.rb
@@ -5,8 +5,18 @@ describe InvitationPresenter do
   let(:invitation_presenter) { InvitationPresenter.new(invitation) }
 
   it '#member' do
-    expect(MemberPresenter).to receive(:new).with(invitation.member)
+    expect(invitation_presenter.member).to eq(invitation.member)
+  end
 
-    invitation_presenter.member
+  context '#attendance_status' do
+    it 'returns Attending when attending' do
+      invitation.update_attribute(:attending, true)
+
+      expect(invitation_presenter.attendance_status).to eq('Attending')
+    end
+
+    it 'returns RSVP when not attending' do
+      expect(invitation_presenter.attendance_status).to eq('RSVP')
+    end
   end
 end
