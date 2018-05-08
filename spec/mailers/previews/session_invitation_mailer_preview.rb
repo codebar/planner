e79b60158145faca307864318d57170dac55a0d2
matyikriszta <matyi.kriszta@gmail.com>
2018-05-08 10:01:12 -0700

    Renames session_invitation_mailer preview to workshop_invitation_mailer

--- spec/mailers/previews/session_invitation_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-class SessionInvitationMailerPreview < ActionMailer::Preview
-  def invite_coach
-    SessionInvitationMailer.invite_coach(Workshop.first, Member.last, SessionInvitation.first)
-  end
-
-  def invite_student
-    SessionInvitationMailer.invite_coach(Workshop.first, Member.first, SessionInvitation.first)
-  end
-end
