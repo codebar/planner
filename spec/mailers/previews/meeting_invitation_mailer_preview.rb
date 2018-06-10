abbe600d6d288e06fa5fd6d1eb060b1bf6857838
matyikriszta <matyi.kriszta@gmail.com>
2018-06-09 20:41:53 -0700

    Adds attending, approve_from_waitlist, and attendance_reminder emails to meeting_invitation_mailer preview

--- spec/mailers/previews/meeting_invitation_mailer_preview.rb
+++ spec/mailers/previews/meeting_invitation_mailer_preview.rb
@@ -1,5 +1,17 @@
 class MeetingInvitationMailerPreview < ActionMailer::Preview
   def invite
-    MeetingInvitationMailer.invite(Meeting.first, Member.last)
+    MeetingInvitationMailer.invite(Meeting.last, Member.last)
+  end
+
+  def attending
+    MeetingInvitationMailer.attending(Meeting.last, Member.last)
+  end
+
+  def approve_from_waitlist
+    MeetingInvitationMailer.approve_from_waitlist(Meeting.last, Member.last)
+  end
+
+  def attendance_reminder
+    MeetingInvitationMailer.attendance_reminder(Meeting.last, Member.last)
   end
 end
