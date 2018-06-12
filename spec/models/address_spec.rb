a4aa79531f63d4f50e66a9822b669e4e18cb4302
matyikriszta <matyi.kriszta@gmail.com>
2018-06-11 21:47:46 -0700

    Test address should respond to directions

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,5 +6,9 @@ describe Address do
   it { should respond_to(:sponsor) }
   it { should respond_to(:flat) }
   it { should respond_to(:street) }
+  it { should respond_to(:city) }
+  it { should respond_to(:latitude) }
+  it { should respond_to(:longitude) }
   it { should respond_to(:postal_code) }
+  it { should respond_to(:directions) }
 end
