761b43dcae84f37b84a1c950b5c86160ffc3dab0
matyikriszta <matyi.kriszta@gmail.com>
2018-06-11 18:51:41 -0700

    Adds previews for invite_student, invite_coaches, attending, attending_reminder, waiting_list_reminder and notify_waiting_list workshop invitation emails

--- spec/mailers/previews/workshop_invitation_mailer_preview.rb
+++ spec/mailers/previews/workshop_invitation_mailer_preview.rb
@@ -1,9 +1,25 @@
 class WorkshopInvitationMailerPreview < ActionMailer::Preview
   def invite_coach
-    WorkshopInvitationMailer.invite_coach(Workshop.first, Member.last, WorkshopInvitation.first)
+    WorkshopInvitationMailer.invite_coach(Workshop.last, Member.last, WorkshopInvitation.first)
   end
 
   def invite_student
-    WorkshopInvitationMailer.invite_coach(Workshop.first, Member.first, SessionInvitation.first)
+    WorkshopInvitationMailer.invite_student(Workshop.last, Member.last, WorkshopInvitation.first)
+  end
+
+  def attending
+    WorkshopInvitationMailer.attending(Workshop.last, Member.last, WorkshopInvitation.first)
+  end
+
+  def attending_reminder
+    WorkshopInvitationMailer.attending_reminder(Workshop.last, Member.last, WorkshopInvitation.first)
+  end
+
+  def waiting_list_reminder
+    WorkshopInvitationMailer.waiting_list_reminder(Workshop.last, Member.last, WorkshopInvitation.first)
+  end
+
+  def notify_waiting_list
+    WorkshopInvitationMailer.notify_waiting_list(WorkshopInvitation.first)
   end
 end
