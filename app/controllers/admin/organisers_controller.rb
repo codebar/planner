a3687a0afa70cffd6aac1767c766e75d10a2f6f7
matyikriszta <matyi.kriszta@gmail.com>
2018-08-02 13:42:33 -0700

    Adds Admin::OrganisersController

---
+++ app/controllers/admin/organisers_controller.rb
@@ -0,0 +1,34 @@
+class Admin::OrganisersController < Admin::ApplicationController
+  before_action :set_chapter, only: %i[index create destroy]
+
+  def index
+    authorize :organiser
+
+    @organisers = @chapter.organisers
+    @chapter_members = @chapter.members
+  end
+
+  def create
+    authorize :organiser
+
+    member = Member.find(params[:organiser][:organiser])
+    member.add_role(:organiser, @chapter)
+
+    redirect_to admin_chapter_organisers_path(@chapter), notice: 'Successfully added organiser.'
+  end
+
+  def destroy
+    authorize :organiser
+    
+    member = Member.find(params[:id])
+
+    member.remove_role(:organiser, @chapter)
+    redirect_to admin_chapter_organisers_path(@chapter), notice: 'Successfully removed organiser.'
+  end
+
+  private
+
+  def set_chapter
+    @chapter = Chapter.find(params[:chapter_id])
+  end
+end
