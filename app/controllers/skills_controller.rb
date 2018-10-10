448ead6b2cdf5c71cf36f67df17a649e80f01eee
Dan Pope <danielthepope@users.noreply.github.com>
2018-10-10 21:58:21 +0100

    Fix indentation identified by Rubocop

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,8 +2,8 @@ class SkillsController < ApplicationController
   def show
     @skill = skill
     @coaches = Member.includes(:skills)
-                      .with_skill(skill)
-                      .paginate(page: page)
+                     .with_skill(skill)
+                     .paginate(page: page)
   end
 
   private
