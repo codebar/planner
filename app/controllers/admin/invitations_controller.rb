4bacbe5a489e3f4c36c08cc3b07f79959bac8ec8
matyikriszta <matyi.kriszta@gmail.com>
2018-10-13 16:45:29 -0700

    Fixes Use the return of the conditional for variable assignment and comparison rubocop offense in admin/invitations_controller.rb

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -43,15 +43,15 @@ class Admin::InvitationsController < Admin::ApplicationController
   private
 
   def set_invitation
-    if params.key?(:workshop)
-      @invitation = WorkshopInvitation.where(
-        workshop_id: @workshop.id,
-        member_id: params[:workshop][:invitations],
-        role: params[:role]
-      ).first_or_create
-    else
-      @invitation = @workshop.invitations.find_by(token: invitation_id)
-    end
+    @invitation = if params.key?(:workshop)
+                    WorkshopInvitation.where(
+                      workshop_id: @workshop.id,
+                      member_id: params[:workshop][:invitations],
+                      role: params[:role]
+                    ).first_or_create
+                  else
+                    @invitation = @workshop.invitations.find_by(token: invitation_id)
+                  end
   end
 
   def invitation_id
