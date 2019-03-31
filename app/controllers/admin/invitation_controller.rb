eef5bd9de29dcfed98a5ff9e44affa607fb08620
Kristján Oddsson <koddsson@gmail.com>
2019-03-31 10:49:57 -0400

    reduce methods sizes by moving DB fetching into `before_action`

--- app/controllers/admin/invitation_controller.rb
+++ app/controllers/admin/invitation_controller.rb
@@ -1,33 +1,36 @@
 class Admin::InvitationController < Admin::ApplicationController
   include Admin::WorkshopConcerns
 
-  # event invitations
+  before_action :fetch_current_invitation, only: %i[update verify cancel]
 
   def update
-    invitation = Invitation.find_by(token: params[:invitation][:id])
-    invitation.update(attending: true, verified: true, verified_by: current_user)
+    @invitation.update(attending: true, verified: true, verified_by: current_user)
 
-    EventInvitationMailer.attending(invitation.event, invitation.member, invitation).deliver_now
+    EventInvitationMailer.attending(@invitation.event, @invitation.member, @invitation).deliver_now
 
-    flash[:notice] = "You have verified #{invitation.member.full_name}'s spot at the event!"
+    flash[:notice] = "You have verified #{@invitation.member.full_name}'s spot at the event!"
     redirect_back(fallback_location: root_path)
   end
 
   def verify
-    invitation = Invitation.find_by(token: params[:invitation_id])
-    invitation.update(verified: true, verified_by_id: current_user.id)
+    @invitation.update(verified: true, verified_by_id: current_user.id)
 
-    EventInvitationMailer.attending(invitation.event, invitation.member, invitation).deliver_now
+    EventInvitationMailer.attending(@invitation.event, @invitation.member, @invitation).deliver_now
 
-    flash[:notice] = "You have verified #{invitation.member.full_name}'s spot at the event!"
+    flash[:notice] = "You have verified #{@invitation.member.full_name}'s spot at the event!"
     redirect_back(fallback_location: root_path)
   end
 
   def cancel
-    invitation = Invitation.find_by(token: params[:invitation_id])
-    invitation.update(attending: false)
+    @invitation.update(attending: false)
 
-    flash[:notice] = "You have cancelled #{invitation.member.full_name}'s attendance."
+    flash[:notice] = "You have cancelled #{@invitation.member.full_name}'s attendance."
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def fetch_current_invitation
+    @invitation = Invitation.find_by(token: params[:invitation_id])
   end
 end
