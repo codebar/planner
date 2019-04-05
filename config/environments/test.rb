cbb4530cbd9b7410a8fe97eeca73b6d1bcc6776f
Kristján Oddsson <koddsson@gmail.com>
2019-04-05 21:04:20 +0100

    bin/rails app:update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
+    'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -39,6 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
+
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
