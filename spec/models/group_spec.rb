4907a3f0fb0f87ca471bc67d64face46eed12c68
Richard <richard.wigley@gmail.com>
2019-09-12 16:54:46 +0100

    Bumps shoulda-matchers from 3.1.3 to 4.1.2 - fix broken spec
      - major version bump
      - spec fixed
        - by including message
        - spec should write the allowed names in the test. If you just
          plug the NAMES array from the class into the expected array
          of the test how can it ever fail? So what's the point?

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -14,6 +14,10 @@ describe Group, type: :model do
 
   context 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_inclusion_of(:name).in_array(Group::NAMES) }
+    it do
+      should validate_inclusion_of(:name).
+        in_array(%w(Coaches Students)).
+        with_message('Invalid name for Group')
+    end
   end
 end
