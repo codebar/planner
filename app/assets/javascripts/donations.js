bea8e49995301ea1566f8625d4fe5f0981e10e77
Kriszta Matyi <matyi.kriszta@gmail.com>
2019-10-10 21:07:33 -0700

    Sends recaptcha token to controller when submitting Stripe checkout form

--- app/assets/javascripts/donations.js
+++ app/assets/javascripts/donations.js
@@ -1,38 +1,49 @@
 $(function() {
   var handler = StripeCheckout.configure({
-   key: stripePublishableKey,
-   image: "https://avatars1.githubusercontent.com/u/5642384?v=3&s=300",
-   currency: 'GBP',
-   token: function(token) {
-     var name = $('#donation_name').val();
-     var amount = $('#donation_amount').val();
-     $.ajax({
-       type: "POST",
-       url: '/donations',
-       data: { amount: amount*100, name: name, data: token }
-     }).done(function(response) {
-       $('.donation-container').html(response);
-     }).fail(function(xhr, status,  e){
-       $('.message').html("Your transaction has not been succesful. Please try again.");
-     });
-   }
+    key: stripePublishableKey,
+    image: "https://avatars1.githubusercontent.com/u/5642384?v=3&s=300",
+    currency: 'GBP',
+    token: function(token) {
+      var name = $('#donation_name').val();
+      var amount = $('#donation_amount').val();
+      window.executeRecaptchaForDonateAsync().then((recaptchaToken) => {
+        $.ajax({
+          type: "POST",
+          url: "/donations",
+          data: {
+            amount: amount * 100,
+            name: name,
+            data: token,
+            'g-recaptcha-response[donate]': recaptchaToken
+          }
+        })
+        .done(function(response) {
+          $(".donation-container").html(response);
+        })
+        .fail(function(xhr, status, e) {
+          $(".message").html(
+            "Your transaction has not been succesful. Please try again."
+          );
+        });
+      });
+    }
   });
 
   $('#donate').on('click', function(e) {
-   var amount = $('#donation_amount').val();
-   if (!$.isNumeric(amount)) {
-     $('.message').html("You have not entered a valid amount.");
-     return;
-   }
+    var amount = $('#donation_amount').val();
+    if (!$.isNumeric(amount)) {
+      $('.message').html("You have not entered a valid amount.");
+      return;
+    }
 
-   $('.message').html("");
+    $('.message').html("");
 
-   handler.open({
-     name: 'codebar',
-     description: 'Donation of £' + amount,
-     amount: amount*100
-   });
-   e.preventDefault();
+    handler.open({
+      name: 'codebar',
+      description: 'Donation of £' + amount,
+      amount: amount*100
+    });
+    e.preventDefault();
   });
 
   $(window).on('popstate', function() {
