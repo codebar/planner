1cd50dc36b747a7e2da5e3bca0d6b1eb604b511c
Richard <richard.wigley@gmail.com>
2019-10-18 11:44:56 +0100

    RSpec mailer - remove describe monkey patch, add metadata type
      - describe => RSpec.describe removes monkey patching version
        of describe
      - adding metadata type: :mailer
        - RSpec 3 no longer relies on using the directory structure to
          guess what type of test is in there.

--- spec/mailers/feedback_request_mailer_spec.rb
+++ spec/mailers/feedback_request_mailer_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe FeedbackRequestMailer do
+RSpec.describe FeedbackRequestMailer, type: :mailer  do
   let(:email) { ActionMailer::Base.deliveries.last }
   let(:workshop) { Fabricate(:workshop, title: 'HTML & CSS') }
   let(:member) { Fabricate(:member) }
