fd851ff24675275a309fc46c053357c86a70c07a
Richard <richard.wigley@gmail.com>
2020-03-02 18:28:25 +0000

    (refactor) ChapterController#show has too many lines

--- app/controllers/chapter_controller.rb
+++ app/controllers/chapter_controller.rb
@@ -1,18 +1,12 @@
 class ChapterController < ApplicationController
   def show
-    chapter = Chapter.active.find_by!(slug: slug)
-    @chapter = ChapterPresenter.new(chapter)
+    @chapter = ChapterPresenter.new(Chapter.active.find_by!(slug: slug))
 
-    events = @chapter.upcoming_workshops.includes(:sponsors).sort_by(&:date_and_time).group_by(&:date)
-    @upcoming_workshops = events.map.inject({}) do |hash, (key, value)|
-      hash[key] = EventPresenter.decorate_collection(value)
-      hash
-    end
+    upcoming_events = @chapter.upcoming_workshops.includes(:sponsors).sort_by(&:date_and_time).group_by(&:date)
+    @upcoming_workshops = event_presenters_by_date(upcoming_events)
     past_event = @chapter.workshops.most_recent
     past_events = past_event.present? ? [past_event].group_by(&:date) : []
-    @latest_workshops = past_events.map.inject({}) do |hash, (key, value)|
-      hash[key] = EventPresenter.decorate_collection(value); hash
-    end
+    @latest_workshops = event_presenters_by_date(past_events)
     @recent_sponsors = Sponsor.recent_for_chapter(@chapter)
   end
 
@@ -20,5 +14,12 @@ class ChapterController < ApplicationController
 
   def slug
     params.permit(:id)[:id]
+  end
+
+  def event_presenters_by_date(events)
+    events.map.inject({}) do |hash, (date, value)|
+      hash[date] = EventPresenter.decorate_collection(value)
+      hash
+    end
   end
 end
