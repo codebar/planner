cb968079a41f17230984a64de4257ea05c87ad0c
Richard <richard.wigley@gmail.com>
2020-03-10 18:37:35 +0000

    Localise WorkshopInvitationMailer email subjects

--- app/mailers/workshop_invitation_mailer.rb
+++ app/mailers/workshop_invitation_mailer.rb
@@ -9,7 +9,8 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = member
     @invitation = invitation
 
-    subject = "Workshop Invitation #{humanize_date(@workshop.date_and_time, with_time: true)}"
+    subject = t('mailer.workshop_invitation.invite_student.subject',
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
 
     mail(mail_args(member, subject, 'no-reply@codebar.io'), &:html)
   end
@@ -19,7 +20,8 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = member
     @invitation = invitation
 
-    subject = "Workshop Coach Invitation #{humanize_date(@workshop.date_and_time, with_time: true)}"
+    subject = t('mailer.workshop_invitation.invite_coach.subject',
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
 
     mail(mail_args(member, subject, 'no-reply@codebar.io'), &:html)
   end
@@ -31,7 +33,8 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @invitation = invitation
     @waiting_list = waiting_list
 
-    subject = "Attendance Confirmation for #{humanize_date(@workshop.date_and_time, with_time: true)}"
+    subject = t('mailer.workshop_invitation.attending.subject',
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
 
     attachments['codebar.ics'] = { mime_type: 'text/calendar',
                                    content: WorkshopCalendar.new(workshops).calendar.to_ical }
@@ -46,7 +49,10 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = member
     @invitation = invitation
 
-    subject = "#{title}: #{@workshop.title} by codebar - #{humanize_date(@workshop.date_and_time, with_time: true)}"
+    subject = t('mailer.workshop_invitation.change_of_details.subject',
+                title: title,
+                workshop_title: @workshop.title,
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
 
     mail(mail_args(member, subject, @workshop.chapter.email), &:html)
   end
@@ -57,7 +63,8 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = member
     @invitation = invitation
 
-    subject = "Workshop Reminder #{humanize_date(@workshop.date_and_time, with_time: true)}"
+    subject = t('mailer.workshop_invitation.attending_reminder.subject',
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
     mail(mail_args(member, subject, @workshop.chapter.email), &:html)
   end
 
@@ -67,7 +74,8 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = member
     @invitation = invitation
 
-    subject = "Reminder: you're on the codebar waiting list (#{humanize_date(@workshop.date_and_time, with_time: true)})"
+    subject = t('mailer.workshop_invitation.waiting_list_reminder.subject',
+                date_time: humanize_date(@workshop.date_and_time, with_time: true))
     mail(mail_args(member, subject, @workshop.chapter.email), &:html)
   end
 
@@ -77,7 +85,7 @@ class WorkshopInvitationMailer < ActionMailer::Base
     @member = invitation.member
     @invitation = invitation
 
-    subject = 'A spot just became available'
+    subject = t('mailer.workshop_invitation.notify_waiting_list.subject')
 
     mail(mail_args(@member, subject, @workshop.chapter.email), &:html)
   end
--- spec/mailers/workshop_invitation_mailer_spec.rb
+++ spec/mailers/workshop_invitation_mailer_spec.rb
@@ -5,24 +5,57 @@ RSpec.describe WorkshopInvitationMailer, type: :mailer  do
   let(:workshop) { Fabricate(:workshop, title: 'HTML & CSS') }
   let(:member) { Fabricate(:member) }
   let(:invitation) { Fabricate(:workshop_invitation, workshop: workshop, member: member) }
+  let(:sponsor) { Fabricate(:sponsor) }
 
   it '#invite_student' do
     email_subject = "Workshop Invitation #{humanize_date(workshop.date_and_time, with_time: true)}"
+
     WorkshopInvitationMailer.invite_student(workshop, member, invitation).deliver_now
 
     expect(email.subject).to eq(email_subject)
     expect(email.body.encoded).to match(workshop.chapter.email)
   end
 
+  it '#invite_coach' do
+    email_subject = "Workshop Coach Invitation #{humanize_date(workshop.date_and_time, with_time: true)}"
+
+    WorkshopInvitationMailer.invite_coach(workshop, member, invitation).deliver_now
+
+    expect(email.subject).to eq(email_subject)
+    expect(email.body.encoded).to match(workshop.chapter.email)
+  end
+
+  it "#attending" do
+    email_subject = "Attendance Confirmation for #{humanize_date(workshop.date_and_time, with_time: true)}"
+
+    WorkshopInvitationMailer.attending(workshop, member, invitation).deliver_now
+
+    expect(email.subject).to eq(email_subject)
+    expect(email.body.encoded).to match(workshop.chapter.email)
+  end
+
+  it "#change_of_details" do
+    title = 'Change of details'
+    email_subject = "#{title}: #{workshop.title} by codebar - #{humanize_date(workshop.date_and_time, with_time: true)}"
+
+    WorkshopInvitationMailer.change_of_details(workshop, sponsor, member, invitation).deliver_now
+
+    expect(email.subject).to eq(email_subject)
+    expect(email.from).to eq([workshop.chapter.email])
+  end
+
   it '#attending_reminder' do
     email_subject = "Workshop Reminder #{humanize_date(workshop.date_and_time, with_time: true)}"
+
     WorkshopInvitationMailer.attending_reminder(workshop, member, invitation).deliver_now
+
     expect(email.subject).to eq(email_subject)
     expect(email.body.encoded).to match(workshop.chapter.email)
   end
 
   it '#waitlist_reminder' do
     email_subject = "Reminder: you're on the codebar waiting list (#{humanize_date(workshop.date_and_time, with_time: true)})"
+
     WorkshopInvitationMailer.waiting_list_reminder(workshop, member, invitation).deliver_now
 
     expect(email.subject).to eq(email_subject)
