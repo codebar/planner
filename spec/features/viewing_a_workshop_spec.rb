0702b34eaff0fdc83dfbd09356bb5e492c7f0bf1
Despo Pentara <despo@extractmethod.com>
2020-03-12 22:14:34 +0000

    (testing) add tests for workshop view and tweaks to view to make testing easier

--- spec/features/viewing_a_workshop_spec.rb
+++ spec/features/viewing_a_workshop_spec.rb
@@ -1,23 +1,43 @@
 require 'spec_helper'
 RSpec.feature 'Viewing a workshop page', type: :feature do
   context 'visitor' do
-    before(:each) do
+    before do
       visit workshop_path(workshop)
     end
 
-    let(:workshop) { Fabricate(:workshop) }
+    context 'workshop' do
+      let(:workshop) { Fabricate(:workshop) }
 
-    scenario 'can view a workshop' do
-      expect(page).to be
-    end
+      describe '#details' do
+        scenario 'workshop and page title' do
+          expect(page).to have_title("Workshop at #{workshop.host.name} - #{humanize_date(workshop.date_and_time)}")
+          expect(page).to have_content("Workshop at #{workshop.host.name}")
+        end
 
-    scenario 'can view the page title' do
-      expect(page).to have_title("Workshop at #{workshop.host.name} - #{humanize_date(workshop.date_and_time)}")
-    end
+        scenario 'venue name and address' do
+          within '#venue' do
+            expect(page).to have_content(workshop.host.name)
 
-    scenario 'can sign up or sign in' do
-      expect(page).to have_content('Sign up')
-      expect(page).to have_content('Log in')
+            within '#address' do
+              expect(page).to have_content(workshop.host.address.street)
+              expect(page).to have_content(workshop.host.address.city)
+            end
+          end
+        end
+
+        scenario 'sponsor name' do
+          within '#sponsors' do
+            expect(page).to have_content(workshop.host.name)
+          end
+        end
+      end
+
+      describe '#actions' do
+        scenario 'signing up or signing in' do
+          expect(page).to have_content('Sign up')
+          expect(page).to have_content('Log in')
+        end
+      end
     end
   end
 end
