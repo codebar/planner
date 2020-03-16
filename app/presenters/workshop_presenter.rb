f2629a4690ee97e17272da7a8e74e3b049cdcc49
Despo Pentara <despo@extractmethod.com>
2020-03-16 19:09:00 +0000

    (refactoring) WorkshopPresenter improvements
    - better variable names
    - remove obsolete method (there are no references to this)
    - use single quotes
    - extract code from `attendee_array` for better readability

--- app/presenters/workshop_presenter.rb
+++ app/presenters/workshop_presenter.rb
@@ -5,6 +5,7 @@ class WorkshopPresenter < EventPresenter
 
   def self.decorate(workshop)
     return VirtualWorkshopPresenter.new(workshop) if workshop.virtual?
+
     WorkshopPresenter.new(workshop)
   end
 
@@ -29,25 +30,7 @@ class WorkshopPresenter < EventPresenter
   end
 
   def organisers
-    @organisers ||= model.permissions.find_by(name: 'organiser').members rescue chapter_organisers
-  end
-
-  # Gets an HTML list of the organisers, with mobile numbers if the event's
-  # not past and the user's logged in.
-  def organisers_as_list(logged_in = false)
-    list = organisers.shuffle.map do |o|
-      organiser = ActionController::Base.helpers.link_to(o.full_name, o.twitter_url)
-      organiser << " - #{o.mobile}" if logged_in && model.future? && o.mobile
-      content_tag(:li, organiser)
-    end.join.html_safe
-    if list.blank?
-      list = content_tag(:li, 'Nobody yet')
-    end
-    content_tag(:ul, list)
-  end
-
-  def attendees_csv
-    CSV.generate { |csv| attendee_array.each { |a| csv << a } }
+    @organisers ||= model.permissions&.find_by(name: 'organiser')&.members || chapter_organisers
   end
 
   def attendees_checklist
@@ -84,6 +67,7 @@ class WorkshopPresenter < EventPresenter
 
   def distance_of_time
     return "(#{distance_of_time_in_words_to_now(date_and_time)} ago)" if past?
+
     "(in #{distance_of_time_in_words_to_now(date_and_time)})"
   end
 
@@ -108,32 +92,30 @@ class WorkshopPresenter < EventPresenter
   end
 
   def coaches_checklist
-    model.attending_coaches.each_with_index.map do |a, pos|
-      member_info(a.member, pos).to_s
+    model.attending_coaches.each_with_index.map do |invitation, pos|
+      member_info(invitation.member, pos).to_s
     end.join("\n\n")
   end
 
   def attending_organisers
-    organisers.map do |o|
-      [o.full_name, 'ORGANISER']
-    end
+    organisers.map { |organiser| [organiser.full_name, 'ORGANISER'] }
   end
 
   def attendee_array
-    model.attendances.map do |i|
-      if organisers.include?(i.member)
-        [i.member.full_name, 'ORGANISER']
-      else
-        [i.member.full_name, i.role.upcase]
-      end
+    model.attendances.map do |invitation|
+      map_member_details(organisers, invitation.member, invitation.role)
     end.concat(attending_organisers).uniq
   end
 
   def member_info(member, pos)
-    "#{MemberPresenter.new(member).newbie? ? "I__" : "___"} #{pos + 1}.\t #{member.full_name}"
+    "#{MemberPresenter.new(member).newbie? ? 'I__' : '___'} #{pos + 1}.\t #{member.full_name}"
   end
 
   def note(invitation)
-    "#{invitation.note.present? ? invitation.note : "__________________________"} "
+    invitation.note || '__________________________'
+  end
+
+  def map_member_details(organisers, member, role)
+    organisers.include?(member) ? [member.full_name, 'ORGANISER'] : [member.full_name, role.upcase]
   end
 end
