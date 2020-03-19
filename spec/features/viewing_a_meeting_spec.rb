7a4666555d518f62a315dbe2334a9fd775d7b141
Richard <richard.wigley@gmail.com>
2020-03-19 20:10:30 +0000

    Date format to _humanize_date: Meeting#show
    humanize_date([date], with_time: false, with_year: false) =>
      I18n.l([date], format: humanize_date)

--- spec/features/viewing_a_meeting_spec.rb
+++ spec/features/viewing_a_meeting_spec.rb
@@ -9,7 +9,8 @@ RSpec.feature 'viewing a meeting', type: :feature do
     end
 
     scenario 'can view the page title' do
-      expect(page).to have_title("#{meeting.name} - #{humanize_date(meeting.date_and_time)}")
+      expect(page).to have_title("#{meeting.name} - " \
+                                 "#{I18n.l(meeting.date_and_time, format: :_humanize_date)}")
     end
 
     scenario "can view a meeting's information" do
