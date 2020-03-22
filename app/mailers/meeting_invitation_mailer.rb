e8a7175ff06c09439ee89aed3d9c258d1cf7cb83
Richard <richard.wigley@gmail.com>
2020-03-22 13:59:56 +0000

    (Refactor) meeting_invitation_mailer

--- app/mailers/meeting_invitation_mailer.rb
+++ app/mailers/meeting_invitation_mailer.rb
@@ -5,9 +5,7 @@ class MeetingInvitationMailer < ActionMailer::Base
   helper ApplicationHelper
 
   def invite(meeting, member)
-    @member = member
-    @meeting = meeting
-    @host_address = AddressPresenter.new(@meeting.venue.address)
+    setup(meeting, member)
     @rsvp_url = meeting_url(@meeting)
 
     subject = "You are invited to codebar's #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
@@ -15,9 +13,7 @@ class MeetingInvitationMailer < ActionMailer::Base
   end
 
   def attending(meeting, member)
-    @member = member
-    @meeting = meeting
-    @host_address = AddressPresenter.new(@meeting.venue.address)
+    setup(meeting, member)
     @cancellation_url = meeting_url(@meeting)
 
     subject = "See you at #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
@@ -25,9 +21,7 @@ class MeetingInvitationMailer < ActionMailer::Base
   end
 
   def approve_from_waitlist(meeting, member)
-    @member = member
-    @meeting = meeting
-    @host_address = AddressPresenter.new(@meeting.venue.address)
+    setup(meeting, member)
     @cancellation_url = meeting_url(@meeting)
 
     subject = "A spot opened up for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
@@ -35,9 +29,7 @@ class MeetingInvitationMailer < ActionMailer::Base
   end
 
   def attendance_reminder(meeting, member)
-    @member = member
-    @meeting = meeting
-    @host_address = AddressPresenter.new(@meeting.venue.address)
+    setup(meeting, member)
     @cancellation_url = meeting_url(@meeting)
 
     subject = "Reminder: You have a spot for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
@@ -50,5 +42,11 @@ class MeetingInvitationMailer < ActionMailer::Base
     def full_url_for(path)
       "#{@host}#{path}"
     end
+  end
+
+  def setup(meeting, member)
+    @member = member
+    @meeting = meeting
+    @host_address = AddressPresenter.new(@meeting.venue.address)
   end
 end
