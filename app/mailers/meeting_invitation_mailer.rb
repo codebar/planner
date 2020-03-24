d52adc1743e6dbf0eab83f98be430d9b397bc70e
Richard <richard.wigley@gmail.com>
2020-03-24 19:02:48 +0000

    (Refactor) MeetingInvitationMailer

--- app/mailers/meeting_invitation_mailer.rb
+++ app/mailers/meeting_invitation_mailer.rb
@@ -5,35 +5,31 @@ class MeetingInvitationMailer < ActionMailer::Base
   helper ApplicationHelper
 
   def invite(meeting, member)
-    setup(meeting, member)
-    @rsvp_url = meeting_url(@meeting)
-
-    subject = "You are invited to codebar's #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
-    mail(mail_args(@member, subject), &:html)
+    setup(meeting, member) do
+      @rsvp_url = meeting_url(@meeting)
+      "You are invited to codebar's #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
+    end
   end
 
   def attending(meeting, member)
-    setup(meeting, member)
-    @cancellation_url = meeting_url(@meeting)
-
-    subject = "See you at #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
-    mail(mail_args(@member, subject), &:html)
+    setup(meeting, member) do
+      @cancellation_url = meeting_url(@meeting)
+      "See you at #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
+    end
   end
 
   def approve_from_waitlist(meeting, member)
-    setup(meeting, member)
-    @cancellation_url = meeting_url(@meeting)
-
-    subject = "A spot opened up for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
-    mail(mail_args(@member, subject), &:html)
+    setup(meeting, member) do
+      @cancellation_url = meeting_url(@meeting)
+      "A spot opened up for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
+    end
   end
 
   def attendance_reminder(meeting, member)
-    setup(meeting, member)
-    @cancellation_url = meeting_url(@meeting)
-
-    subject = "Reminder: You have a spot for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
-    mail(mail_args(@member, subject), &:html)
+    setup(meeting, member) do
+      @cancellation_url = meeting_url(@meeting)
+      "Reminder: You have a spot for #{@meeting.name} on #{l(@meeting.date_and_time, format: :humanize_date)}"
+    end
   end
 
   private
@@ -48,5 +44,7 @@ class MeetingInvitationMailer < ActionMailer::Base
     @member = member
     @meeting = meeting
     @host_address = AddressPresenter.new(@meeting.venue.address)
+    subject = yield
+    mail(mail_args(@member, subject), &:html)
   end
 end
