2fc1b8541f6371568f34fc75a053a8521a577a52
Richard <richard.wigley@gmail.com>
2020-03-26 23:11:26 +0000

    Date format to _humanize_date_with_time: admin/portal#index
     - required includes(:chapter) to work
    
    humanize_date([date], with_time: true, with_year: false) =>
      I18n.l([date], format: humanize_date_with_time)

--- app/controllers/admin/portal_controller.rb
+++ app/controllers/admin/portal_controller.rb
@@ -4,7 +4,7 @@ class Admin::PortalController < Admin::ApplicationController
 
     @jobs_pending_approval = Job.where(approved: false, submitted: true).count
     @chapters = Chapter.active.all.order(name: :asc)
-    @workshops = Workshop.upcoming
+    @workshops = Workshop.includes(:chapter).upcoming
     @groups = Group.joins(:chapter).merge(@chapters)
     @subscribers = Subscription.joins(:chapter).merge(@chapters)
                                .ordered.limit(20).includes(:member, :group)
--- spec/features/admin/accessing_portal_spec.rb
+++ spec/features/admin/accessing_portal_spec.rb
@@ -16,13 +16,19 @@ RSpec.feature 'admin portal', type: :feature do
       login_as_admin(member)
     end
 
-    scenario 'can view all active chapters listed' do
+    scenario 'can view all active chapters and upcoming workshops listed' do
       chapter = Fabricate(:chapter_with_groups)
+      workshop = Fabricate.times(2, :workshop)
       inactive_chapter = Fabricate(:chapter_with_groups, active: false)
       visit admin_root_path
 
       chapter.groups.each do |group|
         expect(page).to have_content(group.to_s)
+      end
+
+      workshops.each do |workshop|
+        expect(page).to have_content(workshop.chapter.name)
+        expect(page).to have_content(I18n.l(workshop.date_and_time, format: :humanize_date_with_time))
       end
 
       inactive_chapter.groups.each do |group|
