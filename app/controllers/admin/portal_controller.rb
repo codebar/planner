e2d78637fc886dee570c5203bbac9241216be10f
Richard <richard.wigley@gmail.com>
2020-03-27 21:06:13 +0000

    (Fix) AdminPortalPresenter replaces PortalController assignments
     - Fixes Assignment Branch Condition size for index is too high.

--- app/controllers/admin/portal_controller.rb
+++ app/controllers/admin/portal_controller.rb
@@ -2,12 +2,7 @@ class Admin::PortalController < Admin::ApplicationController
   def index
     authorize :admin_portal
 
-    @jobs_pending_approval = Job.where(approved: false, submitted: true).count
-    @chapters = Chapter.active.all.order(name: :asc)
-    @workshops = Workshop.upcoming
-    @groups = Group.joins(:chapter).merge(@chapters)
-    @subscribers = Subscription.joins(:chapter).merge(@chapters)
-                               .ordered.limit(20).includes(:member, :group)
+    @portal = AdminPortalPresenter.new
   end
 
   def guide
