e5e60756903af7176e52c1157a41c2659b0e7417
Despo Pentara <despo@extractmethod.com>
2020-05-08 16:14:51 +0100

    (refactoring) extract reusable code to methods

--- app/models/workshop_calendar.rb
+++ app/models/workshop_calendar.rb
@@ -14,32 +14,19 @@ class WorkshopCalendar
   private
 
   def setup_event
-    start_date = workshop.date_and_time.strftime('%Y%m%d')
-    start_time = workshop.time.strftime('%H%M')
-    end_time = workshop.ends_at.strftime('%H%M')
-    address = AddressPresenter.new(workshop.host.address)
     calendar.event do |e|
-      e.url = invitation_url
-      e.organizer = workshop.chapter.email.to_s
-      e.dtstart = Time.zone.parse("#{start_date}#{start_time}")
-      e.dtstart = Time.zone.parse("#{start_date}#{start_time}")
-      e.dtend =  Time.zone.parse("#{start_date}#{end_time}")
+      configure(e)
+
       e.summary = I18n.t('workshop.calendar.summary', host_name: workshop.host.name)
-      e.location = address.to_s
+      e.location = address(workshop)
       e.description = I18n.t('workshop.calendar.description', invitation_link: invitation_url)
-      e.ip_class = 'PRIVATE'
     end
   end
 
   def setup_virtual_event
-    start_date = workshop.date_and_time.strftime('%Y%m%d')
-    start_time = workshop.time.strftime('%H%M')
-    end_time = workshop.ends_at.strftime('%H%M')
     calendar.event do |e|
-      e.url = invitation_url
-      e.organizer = workshop.chapter.email.to_s
-      e.dtstart = Time.zone.parse("#{start_date}#{start_time}")
-      e.dtend =  Time.zone.parse("#{start_date}#{end_time}")
+      configure(e)
+
       e.summary = I18n.t('workshop.calendar.summary', host_name: 'Slack')
       e.location = I18n.t('workshop.virtual.calendar.location')
       e.description = I18n.t('workshop.virtual.calendar.description',
@@ -47,7 +34,30 @@ class WorkshopCalendar
                              slack_channel_link: workshop.slack_channel_link,
                              discord_invitation: I18n.t('social_media_links.discord_invitation'))
       e.description << I18n.t('workshop.calendar.description', invitation_link: invitation_url)
-      e.ip_class = 'PRIVATE'
     end
+  end
+
+  def configure(event)
+    event.url = invitation_url
+    event.organizer = workshop.chapter.email.to_s
+    event.dtstart = Time.zone.parse(start_datetime(workshop))
+    event.dtend =  Time.zone.parse(end_datetime(workshop))
+    event.ip_class = 'PRIVATE'
+  end
+
+  def address(workshop)
+    AddressPresenter.new(workshop.host.address).to_s
+  end
+
+  def start_datetime(workshop)
+    date = workshop.date_and_time.strftime('%Y%m%d')
+    start_time = workshop.time.strftime('%H%M')
+    "#{date}#{start_time}"
+  end
+
+  def end_datetime(workshop)
+    date = workshop.date_and_time.strftime('%Y%m%d')
+    end_time = workshop.ends_at.strftime('%H%M')
+    "#{date}#{end_time}"
   end
 end
