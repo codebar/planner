f97b796553a82bd9f61790e83a2ca3e939d064f3
Richard <richard.wigley@gmail.com>
2020-05-24 17:23:37 +0100

    Listable order desc on past and completed_since_yesterday
       #
       # BEST GUESS
       #
     - completed_since_yesterday - looking at the deleted
       spec/models/concerns/listable the single test on
       completed_since_yesterday was descending so I have set its
       sorting to descending.
     - past - spec/models/concerns/listable was only tested against
       Workshop which had a default_sort of date_and_time: :desc
       However, any other class without a default_sort would get
       an ascending sort. I decided that descending made the most
       sense and fixed it.

--- app/models/concerns/listable.rb
+++ app/models/concerns/listable.rb
@@ -3,10 +3,10 @@ module Listable
 
   included do
     scope :upcoming, -> { where('date_and_time >= ?', Time.zone.now).order(date_and_time: :asc) }
-    scope :past, -> { where('date_and_time < ?', Time.zone.now).order(:date_and_time) }
+    scope :past, -> { where('date_and_time < ?', Time.zone.now).order(date_and_time: :desc) }
     scope :completed_since_yesterday, lambda {
       where('date_and_time < ? and date_and_time > ?', Time.zone.now, Time.zone.now - 24.hours)
-        .order(:date_and_time)
+        .order(date_and_time: :desc)
     }
   end
 
