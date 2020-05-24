fef650c178c5701f16556c81e560352f0259a863
Richard <richard.wigley@gmail.com>
2020-05-24 13:15:31 +0100

    Remove course_spec tests which duplicate behaves_like_listable

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -14,27 +14,6 @@ RSpec.describe Course, type: :model  do
     end
   end
 
-  context 'scopes' do
-    let!(:next_course) { Fabricate(:course, date_and_time: Time.zone.now + 2.days) }
-
-    before do
-      Fabricate(:course, date_and_time: Time.zone.now + 1.week)
-      Fabricate(:course, date_and_time: Time.zone.now - 1.week)
-    end
-
-    it '#upcoming' do
-      expect(Course.upcoming.count).to eq(2)
-    end
-
-    it '#past' do
-      expect(Course.past.count).to eq(1)
-    end
-
-    it '#next' do
-      expect(Course.next).to eq(next_course)
-    end
-  end
-
   it '#attending' do
     2.times { Fabricate(:course_invitation, course: course, attending: true) }
     1.times { Fabricate(:course_invitation, course: course, attending: false) }
