bcf58d6d9c44c6080630405c27896bd066ced7d2
Gonçalo Morais <gnclmorais@gmail.com>
2020-11-27 08:43:25 +0000

    Fix mailer preview
    Make sure we have all the arguments necessary for this mailer view.

--- spec/mailers/previews/meeting_invitation_mailer_preview.rb
+++ spec/mailers/previews/meeting_invitation_mailer_preview.rb
@@ -1,6 +1,14 @@
 class MeetingInvitationMailerPreview < ActionMailer::Preview
   def invite
-    MeetingInvitationMailer.invite(Meeting.last, Member.last)
+    meeting = Meeting.last
+    member = Member.last
+
+    # In the real work, MeetingInvitation should have been created already and a
+    # token should have been assigned. The next lines are for testing purposes.
+    invitation = MeetingInvitation.new(meeting: meeting, member: member)
+    invitation.token = "tokenExample28XIcd6IxQ"
+
+    MeetingInvitationMailer.invite(meeting, member, invitation)
   end
 
   def attending
