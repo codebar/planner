c1aa178e514586ee8cbbcdabdc3085b11da66913
Gonçalo Morais <gnclmorais@gmail.com>
2021-01-23 18:12:31 +0000

    Address issues from CodeFactor and Code Climate
    Extract responsibilities to more focused methods.

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -7,32 +7,11 @@ class Admin::InvitationsController < Admin::ApplicationController
 
     set_invitation
 
-    if params.key?(:attending)
-      attending = params[:attending]
-      if attending.eql?('true')
-        if @invitation.update(attending: true, rsvp_time: Time.zone.now, automated_rsvp: true)
-          @workshop.send_attending_email(@invitation) if @workshop.future?
+    message, error = update_attendance(params.slice(:attending, :attended))
 
-          message = "You have added #{@invitation.member.full_name} to the workshop as a #{@invitation.role}."
-        else
-          invitation_update_error = true
-
-          message = "Error adding #{@invitation.member.full_name} as a #{@invitation.role}. #{@invitation.errors.full_messages.to_sentence}."
-        end
-      else
-        @invitation.update_attribute(:attending, false)
-
-        message = "You have removed #{@invitation.member.full_name} from the workshop."
-      end
-
-      unless invitation_update_error
-        waiting_listed = WaitingList.find_by(invitation: @invitation)
-        waiting_listed&.destroy
-      end
-    elsif params.key?(:attended)
-      @invitation.update_attribute(:attended, true)
-
-      message = "You have verified #{@invitation.member.full_name}'s attendace."
+    unless error
+      waiting_listed = WaitingList.find_by(invitation: @invitation)
+      waiting_listed&.destroy
     end
 
     if request.xhr?
@@ -45,6 +24,40 @@ class Admin::InvitationsController < Admin::ApplicationController
   end
 
   private
+
+  def update_attendance(attending:, attended:)
+    return update_to_attended if attended
+
+    if attending.eql? 'true'
+      update_to_attending
+    else
+      update_to_not_attending
+    end
+  end
+
+  def update_to_attended
+    @invitation.update_attribute(:attended, true)
+
+    ["You have verified #{@invitation.member.full_name}'s attendace."]
+  end
+
+  def update_to_attending
+    if @invitation.update(attending: true, rsvp_time: Time.zone.now, automated_rsvp: true)
+      @workshop.send_attending_email(@invitation) if @workshop.future?
+
+      ["You have added #{@invitation.member.full_name} to the workshop as a #{@invitation.role}."]
+    else
+      error_message = @invitation.errors.full_messages.to_sentence
+
+      ["Error adding #{@invitation.member.full_name} as a #{@invitation.role}. #{error_message}.", false]
+    end
+  end
+
+  def update_to_not_attending
+    @invitation.update_attribute(:attending, false)
+
+    ["You have removed #{@invitation.member.full_name} from the workshop."]
+  end
 
   def set_invitation
     @invitation = @workshop.invitations.find_by(token: invitation_id)
