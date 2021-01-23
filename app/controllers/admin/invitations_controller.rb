b9b3faf2aaf4716e9b4342fc3c0f2a88290dfc70
Gonçalo Morais <gnclmorais@gmail.com>
2021-01-23 22:51:36 +0000

    Refactor update action to a different style

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -4,19 +4,12 @@ class Admin::InvitationsController < Admin::ApplicationController
   def update
     set_and_decorate_workshop
     authorize @workshop
-
     set_invitation
 
-    message, error = update_attendance(params.slice(:attending, :attended))
-
-    unless error
-      waiting_listed = WaitingList.find_by(invitation: @invitation)
-      waiting_listed&.destroy
-    end
+    message = update_attendance(attending: params[:attending], attended: params[:attended])
 
     if request.xhr?
       set_admin_workshop_data
-
       render partial: 'admin/workshops/invitation_management'
     else
       redirect_to :back, notice: message
@@ -28,35 +21,50 @@ class Admin::InvitationsController < Admin::ApplicationController
   def update_attendance(attending:, attended:)
     return update_to_attended if attended
 
-    if attending.eql? 'true'
-      update_to_attending
-    else
-      update_to_not_attending
+    result = attending.eql?('true') ? update_to_attending : update_to_not_attending
+    message, error = result.values_at(:message, :error)
+
+    unless error
+      waiting_listed = WaitingList.find_by(invitation: @invitation)
+      waiting_listed&.destroy
     end
+
+    message
   end
 
   def update_to_attended
     @invitation.update_attribute(:attended, true)
 
-    ["You have verified #{@invitation.member.full_name}'s attendace."]
+    "You have verified #{@invitation.member.full_name}’s attendace."
   end
 
   def update_to_attending
-    if @invitation.update(attending: true, rsvp_time: Time.zone.now, automated_rsvp: true)
-      @workshop.send_attending_email(@invitation) if @workshop.future?
+    update_successful = @invitation.update(attending: true, rsvp_time: Time.zone.now, automated_rsvp: true)
 
-      ["You have added #{@invitation.member.full_name} to the workshop as a #{@invitation.role}."]
-    else
-      error_message = @invitation.errors.full_messages.to_sentence
+    {
+      message: update_successful ? attending_successful : attending_failed,
+      error: !update_successful
+    }
+  end
 
-      ["Error adding #{@invitation.member.full_name} as a #{@invitation.role}. #{error_message}.", false]
-    end
+  def attending_successful
+    @workshop.send_attending_email(@invitation) if @workshop.future?
+
+    "You have added #{@invitation.member.full_name} to the workshop as a #{@invitation.role}."
+  end
+
+  def attending_failed
+    "Error adding #{@invitation.member.full_name} as a #{@invitation.role}. "\
+      "#{@invitation.errors.full_messages.to_sentence}."
   end
 
   def update_to_not_attending
     @invitation.update_attribute(:attending, false)
 
-    ["You have removed #{@invitation.member.full_name} from the workshop."]
+    {
+      message: "You have removed #{@invitation.member.full_name} from the workshop.",
+      error: false
+    }
   end
 
   def set_invitation
