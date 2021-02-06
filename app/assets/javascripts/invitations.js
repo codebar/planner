ba5ef76991b2fc557240374edb1502c4772ea586
Gonçalo Morais <gnclmorais@gmail.com>
2021-02-06 22:06:20 +0000

    Improve HTML re-rendering after marked as attended
    This change will slightly improve the time we have to wait when
    checking someone in for a workshop. In order to get faster times,
    the whole technique has to be replaced by something different.
    At the moment, there are a lot of JS events and layout reflows
    which slow down the page considerably.

--- app/assets/javascripts/invitations.js
+++ app/assets/javascripts/invitations.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $(document).on("ajax:success", "a[data-remote]", function(e, data, status, xhr) {
-    $("#invitations").html(xhr.responseText);
+    var $invitations = $("#invitations");
 
-    $('select').chosen(function(){
+    $invitations.html(xhr.responseText);
+
+    $invitations.find('select').chosen(function () {
       allow_single_deselect: true
       no_results_text: 'No results matched'
     });
-    $(document).foundation();
+
+    $invitations.foundation('tooltip', 'reflow');
   });
+  
   $(document).on('change','#workshop_invitations ',function() {
     this.form.submit();
   })
