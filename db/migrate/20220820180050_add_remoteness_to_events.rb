fcadcf35fe58321c69eeca6143af0a1543765ebb
Jaye Hackett <jaye.hackett@gmail.com>
2022-08-22 14:27:27 +0100

    remote -> virtual

--- db/migrate/20220820180050_add_remoteness_to_events.rb
+++ db/migrate/20220820180050_add_remoteness_to_events.rb
@@ -1,5 +1,5 @@
 class AddRemotenessToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :remote, :boolean, null: false, default: false
+    add_column :events, :virtual, :boolean, null: false, default: false
   end
 end
