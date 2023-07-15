dd76f0af93262460c067dfe30e198ae1ce3f7106
Gonçalo Morais <gnclmorais@gmail.com>
2023-07-15 08:01:44 +0200

    Move GitHub auth message outside of tests
    I’ve found this message to be useless while tests are running, it
    clutters the terminal.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if ENV['GITHUB_KEY'].blank? || ENV['GITHUB_SECRET'].blank?
-    warn '*' * 80
-    warn 'WARNING: Missing consumer key or secret. First, register an app with Github'
-    warn 'Check README.md for instructions on how to set up Github Authentication.'
-    warn '*' * 80
     if Rails.env.test?
       provider :github, 'fakekey', 'fakesecret', scope: 'user:email'
+    else
+      warn '*' * 80
+      warn 'WARNING: Missing consumer key or secret. First, register an app with Github'
+      warn 'Check README.md for instructions on how to set up Github Authentication.'
+      warn '*' * 80
     end
   else
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
