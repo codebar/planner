e534994c139c11a0aa4d85cd48be8bb8f3f6a161
asideofcode-dev <133222359+asideofcode-dev@users.noreply.github.com>
2023-07-23 14:10:52 +0000

    Update sprockets gem

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# TODO: consider moving as per upgrade guidance
+# https://github.com/rails/sprockets/blob/main/UPGRADING.md#manifestjs
 Rails.application.config.assets.precompile += %w(payments.js)
