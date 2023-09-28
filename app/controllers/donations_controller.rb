a847271654483f1d1aa9e9e063ac7381333aed8a
Kriszta Matyi <matyi.kriszta@gmail.com>
2023-09-28 11:56:48 -0700

    Fixes broken Donation button redirect

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,5 @@
 class DonationsController < ApplicationController
   def new
-    redirect_to I18n.t('services.donations')
+    redirect_to I18n.t('services.donations'), allow_other_host: true
   end
 end
