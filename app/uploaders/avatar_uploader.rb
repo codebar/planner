5f74b531085e7f573d9e71f2719c148b0e9d0844
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2025-07-19 11:57:29 +0100

    Limit permitted content
    This mitigates CVE-2016-3714 (ImageTragick)

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
+  def content_type_allowlist
+    [/image\//]
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
