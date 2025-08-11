4c575105b4d3a56e5efd7da273eaf9055d348a80
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-08-11 17:43:29 +0100

    Set up feature tests
    These tests will track the full journey for an admin user who wants to add a Member to a workshop

---
+++ spec/features/admin/add_user_to_workshop_spec.rb
@@ -0,0 +1,9 @@
+RSpec.feature 'Add a user to an existing workshop', type: :feature do
+  scenario 'An admin adds a member to a workshop' do
+
+  end
+
+  scenario 'An admin adds multiple members to a workshop' do
+
+  end
+end
