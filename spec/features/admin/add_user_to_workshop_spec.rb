b0eeb87861849216e92b9f5ea631659ca1df5f2f
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-08-12 11:57:01 +0100

    Write first test
    This feature test checks the flow when there's only one matching member
    
    Signed-off-by: jonathan.kerr <3410350+jonodrew@users.noreply.github.com>

--- spec/features/admin/add_user_to_workshop_spec.rb
+++ spec/features/admin/add_user_to_workshop_spec.rb
@@ -1,4 +1,25 @@
-RSpec.feature 'Add a user to an existing workshop', type: :feature do
+RSpec.describe 'Add a user to an existing workshop', type: :feature do
+  let(:member) {Fabricate(:member)}
+
+  let(:juliet) {Fabricate(:member, name: 'Juliet',  surname: 'Capulet')}
+  let(:romeo) {Fabricate(:member, name: 'Romeo', surname: 'Montague')}
+  let(:workshop) {Fabricate(:workshop)}
+
+  before do
+   login_as_admin(member)
+   @start_page = "/admin/workshops/#{workshop.id}"
+  end
+
+  scenario 'An admin searches and gets an exact match', js: true do
+    visit @start_page
+
+    params = {callback: @start_page.to_s}.to_query
+    visit "/admin/member-search?#{params}"
+    fill_in 'Member Name', with: juliet.name
+    click_on 'Search'
+    expect(current_url).to include(@start_page)
+  end
+
   scenario 'An admin adds a member to a workshop' do
 
   end
