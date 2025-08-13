489cc7e324c438f6edb55d482458785dfa72deb0
David M <207745043+davidmillen50@users.noreply.github.com>
2025-08-13 18:00:21 +0100

    Fix: member note policy
    Ensure only admins can update and delete member notes

--- app/policies/member_note_policy.rb
+++ app/policies/member_note_policy.rb
@@ -3,15 +3,11 @@ class MemberNotePolicy < ApplicationPolicy
     user && (user.has_role?(:admin) || user.roles.where(resource_type: 'Chapter').any?)
   end
 
-  def edit?
-    user && (user.has_role?(:admin) || user.roles.where(resource_type: 'Chapter').any?)
-  end
-
   def update?
-    user && (user.has_role?(:admin) || user.roles.where(resource_type: 'Chapter').any?)
+    user&.has_role?(:admin)
   end
 
   def destroy?
-    user && (user.has_role?(:admin) || user.roles.where(resource_type: 'Chapter').any?)
+    user&.has_role?(:admin)
   end
 end
