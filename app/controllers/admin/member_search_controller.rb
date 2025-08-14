32586a71f08af79b2cc119b4c6b50f2cd9d7c193
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-08-14 10:18:48 +0100

    Switch to using local variables only
    Based on feedback from @olleolleolle, I've updated this controller to only use local variables
    
    Signed-off-by: jonathan.kerr <3410350+jonodrew@users.noreply.github.com>

--- app/controllers/admin/member_search_controller.rb
+++ app/controllers/admin/member_search_controller.rb
@@ -2,20 +2,20 @@ class Admin::MemberSearchController < Admin::ApplicationController
   def index
     member_params = params[:member_search] || {}
     name = member_params[:name]
-    @members = name.blank? ? Member.none : Member.find_members(name).select(:id, :name, :surname, :pronouns)
-    @callback_url = member_params[:callback_url] || params[:callback_url] || results_admin_member_search_index_path
-    if @members.size == 1
-      query = { member_pick: { members: [@members.first.id] } }
+    members = name.blank? ? Member.none : Member.find_members(name).select(:id, :name, :surname, :pronouns)
+    callback_url = member_params[:callback_url] || params[:callback_url] || results_admin_member_search_index_path
+    if members.size == 1
+      query = { member_pick: { members: [members.first.id] } }
       query_string = query.to_query
-      callback_url = "#{@callback_url}?#{query_string}"
+      callback_url = "#{callback_url}?#{query_string}"
       redirect_to callback_url and return
     end
 
-    render 'index', locals: { members: @members, callback_url: @callback_url }
+    render 'index', locals: { members: members, callback_url: callback_url }
   end
 
   def results
-    @members = Member.find(params[:member_pick][:members])
-    render 'show', members: @members
+    members = Member.find(params[:member_pick][:members])
+    render 'show', locals: { members: members }
   end
 end
