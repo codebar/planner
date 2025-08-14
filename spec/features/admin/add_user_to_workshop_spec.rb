3bfaf43713314d8584ee9563678d24b222f3d530
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-08-14 13:55:42 +0100

    Force-create juliet
    This ensures there will always be at least two Members in the tests where they're needed
    
    Signed-off-by: jonathan.kerr <3410350+jonodrew@users.noreply.github.com>

--- spec/features/admin/add_user_to_workshop_spec.rb
+++ spec/features/admin/add_user_to_workshop_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe 'Add a user to an existing workshop', js: true, type: :feature do
   let(:member) {Fabricate(:member)}
 
-  let(:juliet) {Fabricate(:member, name: 'Juliet',  surname: 'Capulet')}
-  let(:romeo) {Fabricate(:member, name: 'Romeo', surname: 'Montague')}
+  let!(:juliet) {Fabricate(:member, name: 'Juliet',  surname: 'Capulet')}
+  let!(:romeo) { Fabricate(:member, name: 'Romeo', surname: 'Montague') }
   let(:workshop) {Fabricate(:workshop)}
 
   before do
@@ -20,9 +20,26 @@ RSpec.describe 'Add a user to an existing workshop', js: true, type: :feature do
     expect(page).to have_current_path(@start_page, ignore_query: true)
   end
 
-  # scenario 'An admin adds a member to a workshop' do
-  #   assert false
-  # end
+  scenario 'An admin adds a member to a workshop' do
+    visit @start_page
+
+    params = {callback_url: @start_page.to_s}.to_query
+    visit "/admin/member-search?#{params}"
+    fill_in 'Member Name', with: 'e'
+    click_on 'Search'
+    expect(page).to have_current_path('/admin/member-search/index', ignore_query: true)
+
+    expect(page).to have_content('Romeo Montague')
+    expect(page).to have_unchecked_field('Romeo Montague')
+    check('Romeo Montague')
+    click_button'Take me back'
+
+    expect(page).to have_current_path(@start_page, ignore_query: true)
+    uri = URI.parse(page.current_url)
+    params = Rack::Utils.parse_nested_query(uri.query).with_indifferent_access
+
+    expect(params[:member_pick][:members]).to eq([romeo.id.to_s])
+  end
   #
   # scenario 'An admin adds multiple members to a workshop' do
   #   assert false
