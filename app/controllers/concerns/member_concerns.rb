dc259146a6a1f0791c4134ba0b7f4de786a9de6a
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-08-21 10:31:27 +0100

    add input for specifying other dietary restrictions

--- app/controllers/concerns/member_concerns.rb
+++ app/controllers/concerns/member_concerns.rb
@@ -10,7 +10,8 @@ module MemberConcerns
 
     def member_params
       params.require(:member).permit(
-        :pronouns, :name, :surname, :email, :mobile, :about_you, :skill_list, :newsletter, dietary_restrictions: [],
+        :pronouns, :name, :surname, :email, :mobile, :about_you, :skill_list, :newsletter, :other_dietary_restrictions,
+          dietary_restrictions: [],
       ).tap do |params|
         params[:dietary_restrictions] = params[:dietary_restrictions].reject(&:blank?) if params[:dietary_restrictions]
       end
