c1a65bf3c80b26ea1a9d350b9ece22982e818292
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-08-22 12:33:11 +0100

    focus the "other dietary restrictions" input
    switch focus to the "other dietary restrictions" text input when
    checking the check box for "Other".

--- app/assets/javascripts/dietary-restrictions.js
+++ app/assets/javascripts/dietary-restrictions.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('#member_dietary_restrictions_other').on('change', function () {
-    const $elementToToggle = $('#member_other_dietary_restrictions').parent();
+    const $otherDietaryRestrictions = $('#member_other_dietary_restrictions');
+    const $elementToToggle = $otherDietaryRestrictions.parent();
     if ($elementToToggle.hasClass('d-none')) {
       $elementToToggle.removeClass('d-none').hide().slideDown(50);
+      $otherDietaryRestrictions.focus();
     } else {
       $elementToToggle.slideUp(50, () => $elementToToggle.addClass('d-none'));
     }
