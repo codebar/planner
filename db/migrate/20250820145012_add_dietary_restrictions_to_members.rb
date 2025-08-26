9d479db1bb53c7053523bf24991728f0fd0d8189
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-08-26 15:25:01 +0100

    replace change_table with add_column in migration
    I was running into a `TypeError: can't quote Array (TypeError)`
    error when running this migration and at first I thought using
    `change_table` instead of `add_column` seemed to help.
    
    In actuality the error still occurs when this migration is run as part
    of a sequence of multiple migrations, but the migration runs
    successfully when run on its own.
    
    However, the issue seems to be fixed in Rails 7.1 so:
    
    - can switch this to use `add_column` which is clearer in terms of the
      intent, and
    - wait until our Rails 7.1 upgrade is merged before shipping this

--- db/migrate/20250820145012_add_dietary_restrictions_to_members.rb
+++ db/migrate/20250820145012_add_dietary_restrictions_to_members.rb
@@ -1,8 +1,6 @@
 class AddDietaryRestrictionsToMembers < ActiveRecord::Migration[7.0]
   def change
-    change_table :members do |t|
-      t.enum :dietary_restrictions, enum_type: "dietary_restriction_enum", array: true, default: []
-    end
+    add_column :members, :dietary_restrictions, :enum, enum_type: "dietary_restriction_enum", array: true, default: []
     add_column :members, :other_dietary_restrictions, :string
   end
 end
