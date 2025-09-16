de1cfa92a3ef41fe446f83d88b030bab44dfa743
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-09-16 11:41:16 +0100

    Update migration to reflect current version of Rails
    Co-authored-by: Morgan Roderick <20321+mroderick@users.noreply.github.com>

--- db/migrate/20250820134727_create_dietary_restriction_enum.rb
+++ db/migrate/20250820134727_create_dietary_restriction_enum.rb
@@ -1,4 +1,4 @@
-class CreateDietaryRestrictionEnum < ActiveRecord::Migration[7.0]
+class CreateDietaryRestrictionEnum < ActiveRecord::Migration[7.1]
   def change
     create_enum :dietary_restriction_enum, %w[vegan vegetarian pescetarian halal gluten_free dairy_free other]
   end
