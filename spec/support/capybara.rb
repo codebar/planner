bfa6f5fa84c94c80dbe8a91d26a80097bb8abafb
Morgan Roderick <morgan@roderick.dk>
2026-02-01 19:34:15 +0100

    test: configure Playwright as JavaScript driver
    Replace Selenium Chrome driver with Playwright.
    Supports chromium (default), firefox, and webkit browsers.
    Headless by default, override with PLAYWRIGHT_HEADLESS=false.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,20 +1,13 @@
-Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :playwright do |app|
+  options = {
+    headless: ENV.fetch('PLAYWRIGHT_HEADLESS', 'true') !~ /^(false|no|0)$/i,
+    browser_type: ENV.fetch('PLAYWRIGHT_BROWSER', 'chromium').to_sym
+  }
 
-  # Chrome won't work properly in a Docker container in sandbox mode
-  options.add_argument("no-sandbox")
-
-  # Run headless by default unless CHROME_HEADLESS specified
-  options.add_argument("headless") unless ENV['CHROME_HEADLESS'] =~ /^(false|no|0)$/i
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: options
-  )
+  Capybara::Playwright::Driver.new(app, options)
 end
 
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :playwright
 Capybara.default_max_wait_time = 5
 
 # Silence Capybara server output
