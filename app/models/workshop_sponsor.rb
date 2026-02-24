f3ca21762f3fceda2fa9bf44810983969e987a1b
Morgan Roderick <morgan@roderick.dk>
2026-02-24 23:13:15 +0000

    models: add inverse_of to WorkshopSponsor belongs_to :workshop
    This enables proper Rails association caching when accessing workshop from
    a workshop_sponsor record, reducing redundant queries.

--- app/models/workshop_sponsor.rb
+++ app/models/workshop_sponsor.rb
@@ -1,6 +1,6 @@
 class WorkshopSponsor < ApplicationRecord
   belongs_to :sponsor
-  belongs_to :workshop
+  belongs_to :workshop, inverse_of: :workshop_sponsors
 
   validates :sponsor_id, uniqueness: { scope: :workshop_id, message: :already_sponsoring }
 end
