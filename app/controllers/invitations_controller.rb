6c83d9287c9885d66847bbd06cd242328c25e121
Morgan Roderick <morgan@roderick.dk>
2026-03-10 19:35:53 +0100

    fix: raise RoutingError to return 404 page

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -88,7 +88,7 @@ class InvitationsController < ApplicationController
 
   def set_invitation
     @invitation = Invitation.find_by(token: params[:token])
-    head :not_found unless @invitation
+    raise ActionController::RoutingError, 'Invitation not found' unless @invitation
   end
 
   def load_invitation
