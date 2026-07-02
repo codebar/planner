c176ed17c4902a83cf5683be818bd717df03baa9
Morgan Roderick <morgan@roderick.dk>
2026-07-02 12:07:11 +0200

    feat: add EventCardComponent with fragment caching

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_21_050948) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_02_102700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -481,6 +481,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_21_050948) do
     t.string "description"
     t.string "name"
     t.datetime "updated_at", precision: nil
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "sponsors", id: :serial, force: :cascade do |t|
