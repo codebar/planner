34d85dc5dc6cced92e75368f301838fb280a9ebb
artfuldev <hello@artful.dev>
2026-09-12 01:11:37 +0100

    Scope chapters search-choice assertion to the chapters widget

--- spec/features/admin/manage_event_spec.rb
+++ spec/features/admin/manage_event_spec.rb
@@ -25,7 +25,9 @@ RSpec.feature 'Managing events' do
 
     find_by_id('event_chapter_ids_chosen').click
     find('.add-all-chapters', text: 'Add to all').click
-    expect(page).to have_css('.search-choice', count: Chapter.count)
+    within('#event_chapter_ids_chosen') do
+      expect(page).to have_css('.search-choice', count: Chapter.count)
+    end
 
     click_on 'Save'
 
